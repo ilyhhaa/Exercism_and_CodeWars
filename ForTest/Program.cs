ac3db3b704042abe12d76ae315d0807c3c11cbfa
Ilya Kunitsky <Ilyhhaa@gmail.com>
2023-09-07 17:24:16 +0300

    Add project files.

---
+++ ForTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        
+
+
+
+    }
+
+    
+}
+static class SavingsAccount
+{
+    public static float InterestRate(decimal balance)
+    {
+        if (balance <= 0)
+        {
+            return 3.213f;
+        }
+        if (balance > 0 && balance <= 1000)
+        {
+            return 0.5f;
+        }
+        if (balance > 1000 && balance <= 5000)
+        {
+            return 1.621f;
+        }
+        if (balance > 5000)
+        {
+            return 2.475f;
+        }
+        return 3.213f;
+
+    }
+
+}
