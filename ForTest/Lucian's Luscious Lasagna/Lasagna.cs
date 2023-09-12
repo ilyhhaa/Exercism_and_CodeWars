4cb9fca368628b64f501cfb2356b957e86b5c05c
Ilya Kunitsky <Ilyhhaa@gmail.com>
2023-09-12 16:05:05 +0300

    Lasag

---
+++ ForTest/Lucian's Luscious Lasagna/Lasagna.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForTest.Lucian_s_Luscious_Lasagna
+{
+    internal class Lasagna
+    {
+        public int ExpectedMinutesInOven()
+        {
+            return 40;
+
+        }
+
+        public int RemainingMinutesInOven(int min)
+        {
+            return 40 - min;
+        }
+
+        public int PreparationTimeInMinutes(int layer)
+        {
+            return layer * 2;
+        }
+
+        public int ElapsedTimeInMinutes(int layers, int minutes)
+        {
+            return (layers * 2) + minutes;
+        }
+    }
+}
