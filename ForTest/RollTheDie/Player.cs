0ac3799daec16001389becd73f848a102c421781
Ilya Kunitsky <Ilyhhaa@gmail.com>
2023-09-15 23:07:32 +0300

    Next one

---
+++ ForTest/RollTheDie/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForTest.RollTheDie
+{
+    public class Player
+    {
+        public int RollDie()
+        {
+            throw new NotImplementedException("Please implement the Player.RollDie() method");
+        }
+
+        public double GenerateSpellStrength()
+        {
+            throw new NotImplementedException("Please implement the Player.GenerateSpellStrength() method");
+        }
+    }
+
+}
