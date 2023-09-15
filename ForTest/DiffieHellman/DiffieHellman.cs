d4222ace6ad8fd30eaf4c933ebdf616b8d009b7b
Ilya Kunitsky <Ilyhhaa@gmail.com>
2023-09-15 23:34:32 +0300

    Diffie–Hellman key exchange

---
+++ ForTest/DiffieHellman/DiffieHellman.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForTest.DiffieHellman
+{
+    internal class DiffieHellman
+    {
+        public static BigInteger PrivateKey(BigInteger primeP)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+
+        public static BigInteger PublicKey(BigInteger primeP, BigInteger primeG, BigInteger privateKey)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+
+        public static BigInteger Secret(BigInteger primeP, BigInteger publicKey, BigInteger privateKey)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
