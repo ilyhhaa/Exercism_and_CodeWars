3f0f7274ec24ac32988ac5b8050914a2213f00b5
Ilya Kunitsky <Ilyhhaa@gmail.com>
2023-09-22 09:22:15 +0300

    "WORK WORK WORK"

---
+++ ForTest/DifferenceOfSquares/DifferenceOfSquares.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForTest.DifferenceOfSquares
+{
+    internal class DifferenceOfSquares
+    {
+        public static int CalculateSquareOfSum(int max)
+        {
+            List<int> nums = new List<int>();
+
+            int a=0;
+            int b =1;
+
+            while (b < max)
+            {
+
+                int res = (int)(Math.Pow(a, 2) + (2 * a * b) + Math.Pow(b, 2));
+
+                nums.Add(res);
+
+                a++;
+                b++;
+
+            }
+
+            return nums.Sum();
+
+            //(a + b)^2 = a^2 + 2ab + b^2.
+        }
+
+        public static int CalculateSumOfSquares(int max)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+
+        public static int CalculateDifferenceOfSquares(int max)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -1,13 +1,16 @@
-﻿using ForTest.Interest_is_Interesting;
+﻿using ForTest.DifferenceOfSquares;
+using ForTest.Interest_is_Interesting;
 using System.Security.Cryptography.X509Certificates;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        
-      
-      
+
+       var res = DifferenceOfSquares.CalculateSquareOfSum(5);
+
+        Console.WriteLine(res);
+
 
 
     }
