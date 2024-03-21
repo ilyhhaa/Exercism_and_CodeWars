e68e2228dcf625d274aa76f56f3b25953f259498
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-03-21 18:17:13 +0300

    oneMore

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -496,3 +496,25 @@ public class MinMax
        return new int[2] {lst.Min(),lst.Max()};
     }
 }
+
+
+/*
+ * 
+ * This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not.
+
+About factors
+Factors are numbers you can multiply together to get another number.
+
+2 and 3 are factors of 6 because: 2 * 3 = 6
+
+Note: base is a non-negative number, factor is a positive number.
+ * 
+ */
+
+public class Kata11
+{
+    public static bool CheckForFactor(int num, int factor) => num%factor == 0;
+    
+}
