51b8b624e8a5dec8d48330635cad893c7219b9e9
Ilya <ilyhhaa@gmail.com>
2024-04-11 14:07:00 +0300

    InterestingKata

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -1041,3 +1041,41 @@ public class Kata28
                   
     }
 }
+	/*
+ Make me a window. I'll give you a number (num). You return a window.
+
+Rules:
+
+The window will always be 2 x 2.
+
+The window needs to have N number of periods across and N number of periods vertically in each pane.
+
+Example:
+
+N = 3
+
+---------
+|...|...|
+|...|...|
+|...|...|
+|---+---|
+|...|...|
+|...|...|
+|...|...|
+---------
+Note: there should be no trailing new line characters at the end.
+ */
+
+static class KataWindow
+{
+    public static string MakeAWindow(int num)
+    {
+        string line = new string('.', num);
+        string pane = "|" + string.Join("|", Enumerable.Repeat(line, 2)) + "|";
+        string window = string.Join("\n", Enumerable.Repeat(pane, num)) + "\n" +
+                        "|" + string.Join("+", Enumerable.Repeat(new string('-', num), 2)) + "|\n" +
+                        string.Join("\n", Enumerable.Repeat(pane, num));
+
+        return new string('-', num * 2 + 3) + "\n" + window + "\n" + new string('-', num * 2 + 3);
+    }
+}
