c2aeeefdff3b42fa262e7be982bb3d3de1dedb34
Ilya <ilyhhaa@gmail.com>
2024-04-11 12:58:51 +0300

    OneMoreEsKata

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -1010,3 +1010,11 @@ public static class Kata26
 /*
  Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
  */
+
+    public class DivisibleNb 
+{
+	public static bool IsDivisible(int n, int x, int y) 
+  {
+	  return (n%x==0 &&n%y==0) ? true:false;
+	}
+}
