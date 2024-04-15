5742af3adeb11bad4ea57ecf12947f8198be7503
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-04-15 22:43:29 +0300

    OneMoreKata35

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -1238,3 +1238,21 @@ public class Kata34
         return true;
     }
 }
+
+/*Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+If no occurrences can be found, a count of 0 should be returned.
+
+("Hello", "o")  ==>  1
+("Hello", "l")  ==>  2
+("", "z")       ==>  0
+str_count("Hello", 'o'); // returns 1
+str_count("Hello", 'l'); // returns 2
+str_count("", 'z'); // returns 0*/
+class Kata35
+{
+    public static int StrCount(string str, char letter)
+    {
+        return str.Count(c=>c == letter);
+    }
+}
