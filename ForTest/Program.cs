83fc12eb644ed2ba0a629346ebc1b3d105792082
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-04-23 18:14:46 +0300

    GoodKata

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -1307,3 +1307,26 @@ public class Accumul
     }
 
 
+/*To solve this Kata, complete the function, calculateHypotenuse(a,b), which will return the length of the hyptenuse for a right angled triangle with the other two sides having a length equal to the inputs. More details:
+
+The returned value should be a number rounded to three decimal places
+An error (ArgumentException in C#) should be thrown if an invalid input is provided (inputs should both be numbers that are above zero)
+Kata.CalculateHypotenuse(1,1)           // returns 1.414
+Kata.CalculateHypotenuse(3,4)           // returns 5
+Kata.CalculateHypotenuse(-2,1)          // throws ArgumentException
+Kata.CalculateHypotenuse(2, Double.NaN) // throws ArgumentException*/
+
+public class Kata37
+{
+    public static double CalculateHypotenuse(double a, double b)
+    {
+        if (a <= 0 || b <= 0 || Double.IsNaN(a) || Double.IsNaN(b))
+        {
+            throw new ArgumentException("Inputs should both be numbers that are above zero");
+        }
+
+        double hypotenuse = Math.Sqrt(a * a + b * b);
+
+        return Math.Round(hypotenuse, 3);
+    }
+}
