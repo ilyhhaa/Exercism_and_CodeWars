f800b33d80403881d13938288de0fa241b9a44a7
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-05-07 16:26:03 +0300

    6kyu

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -1523,3 +1523,32 @@ public class Kata41
 
         return totalDivisors;
     }
+
+    /*Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder("AAAABBBCCDAABBB") == {'A', 'B', 'C', 'D', 'A', 'B'}
+uniqueInOrder("ABBCcAD")         == {'A', 'B', 'C', 'c', 'A', 'D'}
+uniqueInOrder([1,2,2,3,3])       == {1,2,3}*/
+
+    public static class Kata42
+    {
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            T prev = default!;
+            bool hasPrev = false;
+
+            foreach (T item in iterable)
+            {
+                if ((!hasPrev && (hasPrev = true)) || !EqualityComparer<T>.Default.Equals(item, prev))
+                {
+                    yield return item;
+                }
+                prev = item;
+            }
+
+        }
+
+    }
+}
