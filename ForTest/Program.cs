780e81b6548ae6723063060e8d602f0ceb1d42c5
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-05-07 16:21:19 +0300

    ok it's not 6kyu

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -1477,3 +1477,49 @@ public class IPConvert
         return new IPAddress(address).ToString();
     }
 }
+/*Count the number of divisors of a positive integer n.
+
+Random tests go up to n = 500000.
+
+Examples (input --> output)
+4 --> 3 // we have 3 divisors - 1, 2 and 4
+5 --> 2 // we have 2 divisors - 1 and 5
+12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+*/
+
+public class Kata41
+{
+    public static int Divisors(int n)
+    {
+        int totalDivisors = 1;
+
+
+        bool[] isPrime = new bool[n + 1];
+        for (int i = 2; i <= n; i++)
+            isPrime[i] = true;
+
+        for (int p = 2; p * p <= n; p++)
+        {
+            if (isPrime[p])
+            {
+                int count = 0;
+                if (n % p == 0)
+                {
+                    while (n % p == 0)
+                    {
+                        n /= p;
+                        count++;
+                    }
+                    totalDivisors *= (count + 1);
+                }
+            }
+        }
+
+        if (n > 1)
+            totalDivisors *= 2;
+
+        return totalDivisors;
+    }
