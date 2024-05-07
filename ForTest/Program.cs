0cfc4f70d64ea133a575abc3f0e5997792880181
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-05-07 16:31:00 +0300

    Regex REGEX OG

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -1552,3 +1552,39 @@ uniqueInOrder([1,2,2,3,3])       == {1,2,3}*/
 
     }
 }
+
+/*Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+
+Example
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;*/
+
+//using System.Text.RegularExpressions;
+public static class Kata43
+{
+    public static int CountSmileys(string[] smileys)
+    {
+        int count = 0;
+
+        foreach (string s in smileys)
+        {
+            if (Regex.IsMatch(s, @"[:;][-~]?[)D]"))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
