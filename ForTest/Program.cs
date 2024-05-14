db8bb258d965467244d0df7401bee71d7afa35ec
Ilya <ilyhhaa@gmail.com>
2024-05-14 12:14:48 +0300

    pain-pain-pain

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -1674,3 +1674,110 @@ public class TwoSumSol
         return ints.ToArray();
     }
 }
+
+/*Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+Example 1:
+
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.*/
+
+public class SolutionValidSud {
+    public bool IsValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++)
+        {
+            if (!IsValidRow(board, i) || !IsValidColumn(board, i) || !IsValidSubgrid(board, i))
+                return false;
+        }
+        return true;
+    }
+
+    static bool IsValidRow(char[][] board, int row)
+    {
+        bool[] seen = new bool[10];
+        for (int j = 0; j < 9; j++)
+        {
+            char c = board[row][j];
+            if (c != '.')
+            {
+                int num = c - '0';
+                if (seen[num])
+                    return false;
+                seen[num] = true;
+            }
+        }
+        return true;
+    }
+
+    static bool IsValidColumn(char[][] board, int col)
+    {
+        bool[] seen = new bool[10];
+        for (int i = 0; i < 9; i++)
+        {
+            char c = board[i][col];
+            if (c != '.')
+            {
+                int num = c - '0';
+                if (seen[num])
+                    return false;
+                seen[num] = true;
+            }
+        }
+        return true;
+    }
+
+    static bool IsValidSubgrid(char[][] board, int subgrid)
+    {
+        bool[] seen = new bool[10];
+        int rowStart = (subgrid / 3) * 3;
+        int colStart = (subgrid % 3) * 3;
+        for (int i = rowStart; i < rowStart + 3; i++)
+        {
+            for (int j = colStart; j < colStart + 3; j++)
+            {
+                char c = board[i][j];
+                if (c != '.')
+                {
+                    int num = c - '0';
+                    if (seen[num])
+                        return false;
+                    seen[num] = true;
+                }
+            }
+        }
+        return true;
+    }
+    }
