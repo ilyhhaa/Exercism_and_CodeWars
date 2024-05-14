c421540973352e72b0313868c6061efc608433e1
Ilya <ilyhhaa@gmail.com>
2024-05-14 14:31:22 +0300

    Update Program.cs

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -1837,3 +1837,13 @@ public class SolutionMovesZero
 
     }
 }
+public class Solution {
+    public void ReverseString(char[] s) {
+        
+       for (int i = 0; i < s.Length / 2; i++) {
+        char temp = s[i];
+        s[i] = s[s.Length - 1 - i];
+        s[s.Length - 1 - i] = temp;
+    }
+    }
+}
