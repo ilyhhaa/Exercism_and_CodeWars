15022f26fe0f03548499913364e207bf4b32d9ad
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-05-16 16:04:37 +0300

    NiceKata

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -1855,4 +1855,90 @@ public class Solution {
     }*/
 
 
-/**/
+/*Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of 
+𝑛
+3
+n 
+3
+ , the cube above will have volume of 
+(
+𝑛
+−
+1
+)
+3
+(n−1) 
+3
+  and so on until the top which will have a volume of 
+1
+3
+1 
+3
+ .
+
+You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+
+The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an integer m and you have to return the integer n such as 
+𝑛
+3
++
+(
+𝑛
+−
+1
+)
+3
++
+(
+𝑛
+−
+2
+)
+3
++
+.
+.
+.
++
+1
+3
+=
+𝑚
+n 
+3
+ +(n−1) 
+3
+ +(n−2) 
+3
+ +...+1 
+3
+ =m if such a n exists or -1 if there is no such n.
+
+Examples:
+findNb(1071225) --> 45
+
+findNb(91716553919377) --> -1*/
+
+public class ASum
+{
+
+    public static long findNb(long m)
+    {
+        long n = 0;
+        long sum = 0;
+        while (sum < m)
+        {
+            n++;
+            sum += n * n * n;
+        }
+        if (sum == m)
+        {
+            return n;
+        }
+        else
+        {
+            return -1;
+        }
+
+    }
+}
