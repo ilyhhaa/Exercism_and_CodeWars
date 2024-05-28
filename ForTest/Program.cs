327a3c95a9bceedc988217e44697ba71afe13d61
Ilya <ilyhhaa@gmail.com>
2024-05-28 21:07:56 +0300

    Update Program.cs

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -11,7 +11,29 @@ using System.Text.RegularExpressions;
 using System.Linq;
 using System.Net;
 
+/*Create a function with two arguments that will return an array of the first n multiples of x.
 
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array or list ( depending on language ).
+
+Examples
+
+countBy(1,10)  should return  {1,2,3,4,5,6,7,8,9,10}
+countBy(2,5)  should return {2,4,6,8,10}*/
+
+public static class EzKata
+{
+  public static int[] CountBy(int x, int n)
+  {
+      int[] result = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = (i + 1) * x;
+        }
+        return result;
+  }
+}
 class Difference
 {
     public static int CalculateSquareOfSum(int max)
