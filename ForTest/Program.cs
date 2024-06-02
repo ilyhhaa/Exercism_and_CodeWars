e51e95f6152a0399fea66734ca922f6cc8c9c291
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-06-02 23:47:35 +0300

    Good Night!

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -12,6 +12,8 @@ using System.Linq;
 using System.Net;
 
 
+//2june goodNight
+
 /*altERnaTIng cAsE <=> ALTerNAtiNG CaSe
 Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
 
