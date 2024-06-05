1f0a285f9a8e28fb18e901342f6a4deeb8415253
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-06-05 22:47:45 +0300

    ???

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -13,6 +13,7 @@ using System.Net;
 
 
 
+
 /*A square of squares
 You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
 
