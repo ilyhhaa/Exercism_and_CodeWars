33ab4402dc6013b97ebb3dc7ba84f2ab3cdecca6
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-06-06 22:44:32 +0300

    GOOOOOOD ONE and HardOne 401 point on codewars грац.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -12,6 +12,36 @@ using System.Linq;
 using System.Net;
 
 
+/*Write the following function:
+
+public static double AreaOfPolygonInsideCircle(double circleRadius, int numberOfSides)
+It should calculate the area of a regular polygon of numberOfSides, number-of-sides, or number_of_sides sides inside a circle of radius circleRadius, circle-radius, or circle_radius which passes through all the vertices of the polygon (such circle is called circumscribed circle or circumcircle).
+
+The answer should be a number rounded to 3 decimal places.
+
+Input :: Output Examples
+
+AreaOfPolygonInsideCircle(3, 3) // returns 11.691
+
+AreaOfPolygonInsideCircle(5.8, 7) // returns 92.053
+
+AreaOfPolygonInsideCircle(4, 5) // returns 38.042
+Note: if you need to use Pi in your code, use the native value of your language unless stated otherwise.*/
+
+
+
+public class CalculatorOOooooomy
+{
+    public static double AreaOfPolygonInsideCircle(double circleRadius, int numberOfSides)
+    {
+        double sideLength = 2 * circleRadius * Math.Sin(Math.PI / numberOfSides);
+        double area = (numberOfSides * Math.Pow(sideLength, 2)) / (4 * Math.Tan(Math.PI / numberOfSides));
+        return Math.Round(area, 3);
+    }
+}
+
+
+
 
 /*The museum of incredibly dull things
 The museum of incredibly dull things wants to get rid of some exhibits. Miriam, the interior architect, comes up with a plan to remove the most boring exhibits. She gives them a rating, and then removes the one with the lowest rating.
