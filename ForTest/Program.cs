7d98463995c7c3f016c2010989587de5c2f19aae
Ilya <ilyhhaa@gmail.com>
2024-06-10 12:26:48 +0300

    it wassss tooooooooooooo HARD

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -11,8 +11,67 @@ using System.Text.RegularExpressions;
 using System.Linq;
 using System.Net;
 
+/*Write a function that accepts a square matrix (N x N 2D array) and returns the determinant of the matrix.
 
-/*Write the following function:
+How to take the determinant of a matrix -- it is simplest to start with the smallest cases:
+
+A 1x1 matrix |a| has determinant a.
+
+A 2x2 matrix [ [a, b], [c, d] ] or
+
+|a  b|
+|c  d|
+has determinant: a*d - b*c.
+
+The determinant of an n x n sized matrix is calculated by reducing the problem to the calculation of the determinants of n matrices ofn-1 x n-1 size.
+
+For the 3x3 case, [ [a, b, c], [d, e, f], [g, h, i] ] or
+
+|a b c|  
+|d e f|  
+|g h i|  
+the determinant is: a * det(a_minor) - b * det(b_minor) + c * det(c_minor) where det(a_minor) refers to taking the determinant of the 2x2 matrix created by crossing out the row and column in which the element a occurs:
+
+|- - -|
+|- e f|
+|- h i|  
+Note the alternation of signs.
+
+The determinant of larger matrices are calculated analogously, e.g. if M is a 4x4 matrix with first row [a, b, c, d], then:
+
+det(M) = a * det(a_minor) - b * det(b_minor) + c * det(c_minor) - d * det(d_minor)*/
+
+public class Matrix
+{
+   public static int Determinant(int[][] matrix)
+   {
+       int n = matrix.Length;
+    if (n == 1)
+        return matrix[0][0];
+
+    int det = 0;
+    for (int j = 0; j < n; j++)
+    {
+        int[][] minor = new int[n - 1][];
+        for (int i = 1; i < n; i++)
+        {
+            minor[i - 1] = new int[n - 1];
+            for (int k = 0, l = 0; k < n; k++)
+            {
+                if (k != j)
+                    minor[i - 1][l++] = matrix[i][k];
+            }
+        }
+
+        int sign = (j % 2 == 0) ? 1 : -1;
+        det += sign * matrix[0][j] * Determinant(minor);
+    }
+
+    return det;
+   }
+
+    
+}/*Write the following function:
 
 public static double AreaOfPolygonInsideCircle(double circleRadius, int numberOfSides)
 It should calculate the area of a regular polygon of numberOfSides, number-of-sides, or number_of_sides sides inside a circle of radius circleRadius, circle-radius, or circle_radius which passes through all the vertices of the polygon (such circle is called circumscribed circle or circumcircle).
