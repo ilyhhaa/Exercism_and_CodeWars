791791a98939adaf338984cec2f72cd5478727a7
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-06-11 23:28:40 +0300

    PAINFUUUUL

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -11,6 +11,36 @@ using System.Text.RegularExpressions;
 using System.Linq;
 using System.Net;
 
+/*The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+maxSequence [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+-- should be 6: [4, -1, 2, 1]
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.*/
+
+
+public static class KataSubarray
+{
+    public static int MaxSequence(int[] arr)
+    {
+        if (arr.Length == 0)
+            return 0;
+
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+            currentSum = Math.Max(arr[i], currentSum + arr[i]);
+            maxSum = Math.Max(maxSum, currentSum);
+        }
+
+        return maxSum;
+
+    }
+}
+
 
 /*Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
 
