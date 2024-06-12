d727141aacad0f98ccc37a7b0671363e431db025
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-06-12 23:15:58 +0300

    Oke, GoodNight

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -12,6 +12,32 @@ using System.Linq;
 using System.Net;
 
 
+/*Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example (Input --> Output):
+
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit, there are 3 multiplications)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2, there are 4 multiplications)
+4 --> 0 (because 4 is already a one-digit number, there is no multiplication)*/
+
+public class KataPersist
+{
+    public static int Persistence(long n)
+    {
+        if (n < 10)
+        {
+            return 0;
+        }
+        long product = 1;
+        while (n > 0)
+        {
+            product *= n % 10;
+            n /= 10;
+        }
+        return 1 + Persistence(product);
+    }
+}
+
 /*The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 
 What if the string is empty? Then the result should be empty object literal, {}.*/
