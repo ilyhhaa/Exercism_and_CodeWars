17227946f3fddc99010df3225a580d4c400af83d
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-06-17 15:34:19 +0300

    Ez one

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -44,12 +44,44 @@ public class KataPartialWordSearching
     return foundStrings.ToArray();
   }
 }
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Main(args);
-       
 
+/*Task
+Given a string str, reverse it and omit all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.*/
+
+public class KataReversLetter
+{
+    public string ReverseLetter(string str)
+    {
+        string result = string.Empty;
+
+        for (int i = str.Length-1; i>=0 ; i--)
+        {
+            if (char.IsLetter(str[i]))
+            {
+                result += str[i];
+            }
+        }
+
+        return result;
+
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+
+
+        }
     }
 }
