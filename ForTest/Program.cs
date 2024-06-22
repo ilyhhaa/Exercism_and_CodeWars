b672a1adda2fc8166b1d6626f6e24a3d9b622e20
Ilya <ilyhhaa@gmail.com>
2024-06-22 14:47:23 +0300

    OneMore

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -26,6 +26,48 @@ internal class Program
         }
     }
 
+
+
+/*Americans are odd people: in their buildings, the first floor is actually the ground floor and there is no 13th floor (due to superstition).
+
+Write a function that given a floor in the american system returns the floor in the european system.
+
+With the 1st floor being replaced by the ground floor and the 13th floor being removed, the numbers move down to take their place. In case of above 13, they move down by two because there are two omitted numbers below them.
+
+Basements (negatives) stay the same as the universal level.
+
+More information here
+
+Examples
+1  =>  0 
+0  =>  0
+5  =>  4
+15  =>  13
+-3  =>  -3*/
+
+
+public static class Kata
+{
+  public static int GetRealFloor(int n)
+  {
+    if (n > 0)
+    {
+        if (n >= 13)
+        {
+            return n - 2;
+        }
+        else
+        {
+            return n - 1;
+        }
+    }
+    else
+    {
+        return n;
+  }
+}
+}
+
 /*Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
 
 Examples
