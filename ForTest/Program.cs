f77844f92298535793af706799034c7272f5345d
Ilya <ilyhhaa@gmail.com>
2024-06-24 17:57:15 +0300

    Update Program.cs

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -25,7 +25,23 @@ internal class Program
 
         }
     }
+/*Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
 
+The order of the sequence has to stay the same.
+
+Examples:
+
+Input -> Output
+[1, 1, 2] -> [1, 2]
+[1, 2, 1, 1, 3, 2] -> [1, 2, 3]*/
+
+public class KataDeleteDuplicates
+  {
+    public int[] distinct(int[] a)
+    {
+      return a.ToHashSet().ToArray();
+    }
+  }
 
 
 /*Americans are odd people: in their buildings, the first floor is actually the ground floor and there is no 13th floor (due to superstition).
