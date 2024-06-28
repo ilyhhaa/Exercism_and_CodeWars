f6cdd5254800d768e5e869787d387c0193db27ad
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-06-28 23:41:55 +0300

    "7Kyu"

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -26,6 +26,33 @@ internal class Program
         }
     }
 
+/*Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+Examples:
+
+* With `name` = "john"  => return "Hello, John!"
+* With `name` = "aliCE" => return "Hello, Alice!"
+* With `name` not given 
+  or `name` = ""        => return "Hello, World!"*/
+
+public static class Kata
+{
+    public static string Hello(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return "Hello, World!";
+        }
+        else
+        {
+            string formattedName = char.ToUpper(name[0]) + name.Substring(1).ToLower();
+            return $"Hello, {formattedName}!";
+        }
+    }
+}
+
 
 /*You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
 
@@ -142,7 +169,7 @@ Examples
 -3  =>  -3*/
 
 
-public static class Kata
+public static class KataGetRealFloor
 {
   public static int GetRealFloor(int n)
   {
