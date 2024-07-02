cdd316c49b47487000e31f26b892079334538bad
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-02 21:18:55 +0300

    i

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -26,6 +26,26 @@ internal class Program
         }
     }
 
+/*What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+Example(Input --> Output)
+
+"apple ban" --> ["apple 5", "ban 3"]
+"you will win" -->["you 3", "will 4", "win 3"]
+Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .*/
+
+
+
+public class AddLengthKata
+{
+    public static string[] AddLength(string str)
+    {
+        string[] words = str.Split();
+        return words.Select(word => $"{word} {word.Length}").ToArray();
+    }
+}
+
+
 /*Task
 Given a square matrix, your task is to reverse the order of elements on both of its longest diagonals.
 
