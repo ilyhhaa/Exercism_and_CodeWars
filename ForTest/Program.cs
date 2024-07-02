8382fb411ef2a31fdbd4d0513cac17d9bf0dfcfa
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-02 21:21:43 +0300

    l

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -26,6 +26,25 @@ internal class Program
         }
     }
 
+
+/*Terminal game move function
+In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+Example:
+move(3, 6) should equal 15*/
+
+public class GameFuckthisGame
+{
+    public static int Move(int position, int roll)
+    {
+        int newPosition = position + (roll * 2);
+
+        return newPosition;
+    }
+}
+
 /*What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
 
 Example(Input --> Output)
