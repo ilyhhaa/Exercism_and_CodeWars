706e06b2f7bbda309238eda57cd2258bcbcb874d
Ilya <ilyhhaa@gmail.com>
2024-07-06 22:58:17 +0300

    Update Program.cs

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -27,6 +27,12 @@ internal class Program
     }
 
 
+/*Please write a function that sums a list, but ignores any duplicated items in the list.
+
+For instance, for the list [3, 4, 3, 6] the function should return 10,
+and for the list [1, 10, 3, 10, 10] the function should return 4.*/
+
+
 /*ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 
 If the function is passed a valid PIN string, return true, else return false.
