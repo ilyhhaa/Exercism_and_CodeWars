b252c63a3f33023d15bb2ba7458d70fbd245d216
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-07 17:16:47 +0300

    Great Tomorrow i'm started new education model Ty Ilya

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -17,17 +17,56 @@ internal class Program
     {
         private static void Main(string[] args)
         {
-        int[] ints = { 1, 1, 2, 3 };
-
-        var a = KataSumNoDuplicates.SumNoDuplicates(ints);
-
-        Console.WriteLine(a);
-
-        Console.ReadLine();
+       
 
         }
     }
 
+
+
+/*Santa believes in fairness and wants to give each giftee a present in turn rather than handing all of an individual's presents to them, then all to the next person, and so on.
+
+Santa would much rather give one to Sarah, one to Mo, one to Kai, one to Charlie, and cycle through until he's out of presents. But if Sarah is only given 10 presents and after the 10th cycle Santa has no more for her, Santa will continue the loop of gift-giving but excluding Sarah.
+
+Write a function which accepts a string[] and returns a string[], re-ordering the random input passed into Santa's defined order.
+
+The output array length will be the same as the input length. The sorted list should be alphabetical by name.
+
+Examples:
+
+input:        "Sarah", "Charlie", "Mo"          
+santa sorted: "Charlie", "Mo", "Sarah"
+
+input:        "Sarah", "Sarah", "Charlie", "Charlie", "Charlie", "Mo", "Mo"
+santa sorted: "Charlie", "Mo", "Sarah", "Charlie", "Mo", "Sarah", "Charlie"*/
+
+public static class SantaKata
+{
+    public static string[] SantaSort(string[] unsortedNames)
+    {
+        var sortedNames = unsortedNames.OrderBy(name => name).ToList();
+
+
+        var nameCounts = sortedNames.GroupBy(name => name)
+                                    .ToDictionary(group => group.Key, group => group.Count());
+
+
+        var result = new List<string>();
+        while (nameCounts.Values.Sum() > 0)
+        {
+            foreach (var name in nameCounts.Keys.OrderBy(name => name))
+            {
+                if (nameCounts[name] > 0)
+                {
+                    result.Add(name);
+                    nameCounts[name]--;
+                }
+            }
+        }
+
+        return result.ToArray();
+    }
+}
 
 /*Please write a function that sums a list, but ignores any duplicated items in the list.
 
