18e77ae5eed599f34d667942ad64a3b1b5e1ff15
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-08 23:28:03 +0300

    kk Окей я сейчас перепишу это более лаконично

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -23,6 +23,36 @@ internal class Program
     }
 
 
+/*Given a starting list/array of data, it could make some statistical sense to know how much each value differs from the average.
+
+If for example during a week of work you have collected 55,95,62,36,48 contacts for your business, it might be interesting to know the total (296), the average (59.2), but also how much you moved away from the average each single day.
+
+For example on the first day you did something less than the said average (55, meaning -4.2 compared to the average), much more in the second day (95, 35.8 more than the average and so on).
+
+The resulting list/array of differences starting from [55, 95, 62, 36, 48] is thus [4.2, -35.8, -2.8, 23.2, 11.2].
+
+Assuming you will only get valid inputs (ie: only arrays/lists with numbers), create a function to do that, rounding each difference to the second decimal digit (this is not needed in Haskell); extra points if you do so in some smart, clever or concise way :)
+
+With Clojure to round use:
+(defn roundTo2 [n] (/ (Math/round (* n 100.0)) 100.0))*/
+
+public class KataDistancesFromAverage
+{
+    public static double[] DistancesFromAverage(int[] input)
+    {
+        List<double> result = new List<double>();  
+
+        foreach (int i in input)
+        {
+            result.Add(Math.Round(input.Average()-i,2));
+        }
+       
+        return result.ToArray();
+       
+
+    }
+}
+
 /*You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 
 #Examples:
