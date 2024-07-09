800e615237955e8e470b8ea6c6afe148048753f0
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-10 00:05:56 +0300

    Who cares///

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -30,6 +30,17 @@ The input will be a lowercase string with no spaces.
 
 Good luck!*/
 
+public static class KataCapitalize
+{
+    public static string[] Capitalize(string s)
+    {
+        var evenChars = new string(s.Select((c, index) => index % 2 == 0 ? char.ToUpper(c) : c).ToArray());
+        var oddChars = new string(s.Select((c, index) => index % 2 != 0 ? char.ToUpper(c) : c).ToArray());
+
+        return new string[] { evenChars, oddChars };
+    }
+}
+
 
 /*Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
 
