bf0053d899f4044cb20dd01d9dc228d5e0523d76
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-15 23:04:46 +0300

    Okey

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -71,7 +71,44 @@ public class Kata_ROT13
 
 }
 
+/*Kids drink toddy.
+Teens drink coke.
+Young adults drink beer.
+Adults drink whisky.
+Make a function that receive age, and return what they drink.
 
+Rules:
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more.
+Examples: (Input --> Output)
+
+13 --> "drink toddy"
+17 --> "drink coke"
+18 --> "drink beer"
+20 --> "drink beer"
+30 --> "drink whisky"*/
+
+
+public class KataPeopleWithAgeDrink
+{
+    public static string PeopleWithAgeDrink(int old)
+    {
+        switch (old)
+        {
+            case var _ when old < 14:
+                return "drink toddy";
+            case var _ when old < 18:
+                return "drink coke";
+            case var _ when old < 21:
+                return "drink beer";
+            default:
+                return "drink whisky";
+        }
+    }
+}
 
 
 /*Story
