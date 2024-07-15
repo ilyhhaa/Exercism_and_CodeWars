4e59d498f2ab1d23c24f873398fe8c069d9a1cba
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-15 22:53:24 +0300

    Закончил работу ухожу в отпуск!

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -68,6 +68,40 @@ public class Kata_ROT13
 
     }
 
+
+
+
+    /*Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1
+The test cases contain numbers only by mistake.
+
+*/
+
+
+    public class KataCorrect
+    {
+        public static string Correct(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+            text = text.Replace('5', 'S')
+                       .Replace('0', 'O')
+                       .Replace('1', 'I');
+
+            return text;
+        }
+    }
+
+
     /*Given an array of integers.
 
 Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
