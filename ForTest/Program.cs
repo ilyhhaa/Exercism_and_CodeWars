c2697bd6edd2a4bc2d7b266ef12658fb050b55c2
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-21 22:51:24 +0300

    /Is it letter???? Algo But with input error checks

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -22,6 +22,21 @@ internal class Program
 }
 
 
+//Is it letter????
+
+public class Kata
+{
+    public static bool IsItLetter(char c)
+    {
+        if (!char.IsLetter(c)) return false;
+        if (char.IsDigit(c)) return false;
+        if (char.IsHighSurrogate(c)) return false;
+        if (char.IsLowSurrogate(c)) return false;
+        return char.IsLetter(c);
+    }
+}
+
+
 /*Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
 
 (In this case, all triangles must have surface greater than 0 to be accepted).
