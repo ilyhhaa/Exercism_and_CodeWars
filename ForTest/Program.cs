f83af958b59875dee47b0069185993feea218f63
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-23 18:41:47 +0300

    Create a function that returns the name of the winner in a fight between two fighters.
    Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
    
    Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
    
    Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
    
    Your function also receives a third argument, a string, with the name of the fighter that attacks first.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -6,6 +6,72 @@ internal class Program
 
     }
 }
+/*Create a function that returns the name of the winner in a fight between two fighters.
+
+Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+Your function also receives a third argument, a string, with the name of the fighter that attacks first.
+
+Example:
+  declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+  
+  Lew attacks Harry; Harry now has 3 health.
+  Harry attacks Lew; Lew now has 6 health.
+  Lew attacks Harry; Harry now has 1 health.
+  Harry attacks Lew; Lew now has 2 health.
+  Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+public class Fighter {
+  public string Name;
+  public int Health, DamagePerAttack;
+  public Fighter(string name, int health, int damagePerAttack) {
+    this.Name = name;
+    this.Health = health;
+    this.DamagePerAttack = damagePerAttack;
+  }
+
+}*/
+
+
+public class Fighter
+{
+    public string Name;
+    public int Health;
+    public int DamagePerAttack;
+
+    public Fighter(string name, int health, int damagePerAttack)
+    {
+        this.Name = name;
+        this.Health = health;
+        this.DamagePerAttack = damagePerAttack;
+    }
+}
+public class KataDeclareWinner
+{
+    public static string declareWinner(Fighter fighter1, Fighter fighter2, string firstAttacker)
+    {
+        Fighter attacker = firstAttacker == fighter1.Name ? fighter1 : fighter2;
+        Fighter defender = firstAttacker == fighter1.Name ? fighter2 : fighter1;
+
+        while (true)
+        {
+            defender.Health -= attacker.DamagePerAttack;
+            if (defender.Health <= 0)
+            {
+                return attacker.Name;
+            }
+
+            // Swap roles
+            Fighter temp = attacker;
+            attacker = defender;
+            defender = temp;
+        }
+    }
+}
+
 /*Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
 
 Square all numbers k (0 <= k <= n) between 0 and n.
