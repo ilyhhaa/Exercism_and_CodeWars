bd47af52610d91b9587cd44d69eb20a2e2ff883f
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-23 18:59:08 +0300

    As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
    The input to the function will be an array of three distinct numbers (Haskell: a tuple).
    
    For example:
    
    gimme([2, 3, 1]) => 0
    2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -6,6 +6,33 @@ internal class Program
 
     }
 }
+
+/*As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+For example:
+
+gimme([2, 3, 1]) => 0
+2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+Another example (just to make sure it is clear):
+
+gimme([5, 10, 14]) => 1
+10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.*/
+
+public class KataGimme
+{
+    public static int Gimme(double[] inputArray)
+    {
+        double[] sortedArray = (double[])inputArray.Clone();
+        Array.Sort(sortedArray);
+        double middleValue = sortedArray[1];
+        return Array.IndexOf(inputArray, middleValue);
+    }
+}
+
+
 /*Create a function that returns the name of the winner in a fight between two fighters.
 
 Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
@@ -63,8 +90,6 @@ public class KataDeclareWinner
             {
                 return attacker.Name;
             }
-
-            // Swap roles
             Fighter temp = attacker;
             attacker = defender;
             defender = temp;
