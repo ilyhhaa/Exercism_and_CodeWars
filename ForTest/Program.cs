f0bd19d303a6194cc9f8809106a3b5dc775f8635
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-24 15:47:38 +0300

    Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
    Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -8,6 +8,59 @@ internal class Program
     }
 }
 
+/*Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+Example:
+"MM"      -> 2000
+"MDCLXVI" -> 1666
+"M"       -> 1000
+"CD"      ->  400
+"XC"      ->   90
+"XL"      ->   40
+"I"       ->    1
+Help:
+Symbol    Value
+I          1
+V          5
+X          10
+L          50
+C          100
+D          500
+M          1,000*/
+public class RomanDecode
+{
+    public static int Solution(string roman)
+    {
+        Dictionary<char, int> romanValues = new Dictionary<char, int>
+    {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+        int total = 0;
+        for (int i = 0; i < roman.Length; i++)
+        {
+            if (i + 1 < roman.Length && romanValues[roman[i]] < romanValues[roman[i + 1]])
+            {
+                total -= romanValues[roman[i]];
+            }
+            else
+            {
+                total += romanValues[roman[i]];
+            }
+        }
+
+        return total;
+    }
+}
+
 /*There is an array with some numbers. All numbers are equal except for one. Try to find it!
 
 findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
