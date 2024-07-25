4de19c8724ee332783ef62adeae37aa87b0e77c9
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-07-25 18:41:47 +0300

    Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
    If you want to know more: http://en.wikipedia.org/wiki/DNA
    
    In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
    
    More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
    
    Example: (input --> output)
    
    "ATTGC" --> "TAACG"
    "GTAT" --> "CATA"

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -7,6 +7,48 @@ internal class Program
 
     }
 }
+/*Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"*/
+
+public class DnaStrand
+{
+    public static string MakeComplement(string dna)
+    {
+        char[] complementaryStrand = new char[dna.Length];
+        for (int i = 0; i < dna.Length; i++)
+        {
+            switch (dna[i])
+            {
+                case 'A':
+                    complementaryStrand[i] = 'T';
+                    break;
+                case 'T':
+                    complementaryStrand[i] = 'A';
+                    break;
+                case 'C':
+                    complementaryStrand[i] = 'G';
+                    break;
+                case 'G':
+                    complementaryStrand[i] = 'C';
+                    break;
+                default:
+                    complementaryStrand[i] = dna[i];
+                    break;
+            }
+        }
+        return new string(complementaryStrand);
+    }
+}
 
 /*Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
 
