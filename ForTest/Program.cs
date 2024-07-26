759b7fa538539fb4af02142ace94710fa1a91fb3
Ilya <ilyhhaa@gmail.com>
2024-07-26 21:40:25 +0300

    RowWeights

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -7,8 +7,47 @@ internal class Program
 
     }
 }
+/*Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
 
-/*You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+Notes
+
+Array size is at least 1.
+All numbers will be positive.
+Input >> Output Examples
+
+rowWeights([13, 27, 49])  ==>  return (62, 27)
+Explanation:
+
+The first element 62 is the total weight of team 1, and the second element 27 is the total weight of team 2.
+
+rowWeights([50, 60, 70, 80])  ==>  return (120, 140)
+Explanation:
+
+The first element 120 is the total weight of team 1, and the second element 140 is the total weight of team 2.
+
+rowWeights([80])  ==>  return (80, 0)
+Explanation:
+
+The first element 80 is the total weight of team 1, and the second element 0 is the total weight of team 2.*/
+
+class KataRowWeights
+{
+    public static int[] RowWeights(int[] a)
+    {
+        int team1Weight = 0;
+    int team2Weight = 0;
+
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (i % 2 == 0)
+            team1Weight += a[i];
+        else
+            team2Weight += a[i];
+    }
+
+    return new int[] { team1Weight, team2Weight };
+    }
+}/*You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
 
 You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
 
