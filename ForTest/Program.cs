f0ed7e677e05580b3d1b990aadf31bd7cbef516e
Ilya <ilyhhaa@gmail.com>
2024-07-27 20:33:37 +0300

    Factorial

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -7,7 +7,19 @@ internal class Program
 
     }
 }
-
+/*Just make Fuctorial func*/
+public static class FactorialFunc
+  {
+    public static ulong Factorial(int n)
+    {
+    ulong result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= (ulong)i;
+    }
+    return result;
+    }
+  }
 /*Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
 
 @ @@ @ @@ @ @@ @ @@ @ @@ @
