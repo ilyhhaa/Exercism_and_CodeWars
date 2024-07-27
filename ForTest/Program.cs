589b77ac8fc2a610b4a3e7f24cd8b2b39c83a3f2
Ilya <ilyhhaa@gmail.com>
2024-07-27 20:18:44 +0300

    Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.  @ @@ @ @@ @ @@ @ @@ @ @@ @  Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads. If there are less than 2 blue beads return 0.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -7,6 +7,25 @@ internal class Program
 
     }
 }
+
+/*Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+@ @@ @ @@ @ @@ @ @@ @ @@ @
+
+Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+If there are less than 2 blue beads return 0.*/
+
+public static class KataCountBeads
+{
+  public static int CountRedBeads(int n)
+  {
+    if (n < 2)
+    {
+        return 0;
+    }
+    return 2 * (n - 1);
+  }
+}
 /*Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
 
 Notes
