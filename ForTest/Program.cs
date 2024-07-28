b6ba89c60eaf8324a67561e79cf08d7db5ebe93d
Ilya <ilyhhaa@gmail.com>
2024-07-28 20:33:01 +0300

    GetEnenNumber

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -7,6 +7,21 @@ internal class Program
 
     }
 }
+/*Starting with .NET Framework 3.5, C# supports a Where (in the System.Linq namespace) method which allows a user to filter arrays based on a predicate. Use the Where method to complete the function given.
+
+Enumerable.Where documentation: https://msdn.microsoft.com/en-us/library/bb534803(v=vs.110).aspx
+
+The solution would work like the following:
+
+Kata.GetEvenNumbers(new int[] {2, 4, 5, 6}) => new int[] {2, 4, 6}*/
+
+public class KataGetEvenNumber
+{
+  public static int[] GetEvenNumbers(int[] numbers)
+  {
+     return numbers.Where(n => n % 2 == 0).ToArray();
+  }
+}
 /*When provided with a letter, return its position in the alphabet.
 
 Input :: "a"
