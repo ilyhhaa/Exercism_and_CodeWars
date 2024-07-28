6ee33c1c183f59c396f6f502e72cbdd5c0471e55
Ilya <ilyhhaa@gmail.com>
2024-07-28 21:37:11 +0300

    You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.  Examples:  strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2  Concatenate the consecutive strings of strarr by 2, we get:  treefoling   (length 10)  concatenation of strarr[0] and strarr[1] folingtrashy ("      12)  concatenation of strarr[1] and strarr[2] trashyblue   ("      10)  concatenation of strarr[2] and strarr[3] blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4] abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]  Two strings are the longest: "folingtrashy" and "abcdefuvwxyz". The first that came is "folingtrashy" so  longest_consec(strarr, 2) should return "folingtrashy".  In the same way: longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta" n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).  Note  consecutive strings : follow one after ano

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -7,6 +7,57 @@ internal class Program
 
     }
 }
+
+/*You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+Examples:
+
+strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+Concatenate the consecutive strings of strarr by 2, we get:
+
+treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+The first that came is "folingtrashy" so 
+longest_consec(strarr, 2) should return "folingtrashy".
+
+In the same way:
+longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
+
+Note
+
+consecutive strings : follow one after another without an interruption
+*/
+
+public class LongestConsecutives 
+{
+    
+    public static string LongestConsec(string[] strarr, int k) 
+    {
+       if (strarr.Length == 0 || k > strarr.Length || k <= 0)
+        {
+            return "";
+        }
+
+        string longest = "";
+        for (int i = 0; i <= strarr.Length - k; i++)
+        {
+            string current = string.Concat(strarr.Skip(i).Take(k));
+            if (current.Length > longest.Length)
+            {
+                longest = current;
+            }
+        }
+
+        return longest;
+    }
+}
 /*Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
 
 The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
