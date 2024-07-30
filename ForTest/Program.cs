8d16b3ec0b9854b6efec20e4040e7b43696b08b5
Ilya <ilyhhaa@gmail.com>
2024-07-30 21:41:24 +0300

    class Kata {     public static int UnusualFive() => "Hello".Length; }

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -8,6 +8,17 @@ internal class Program
     }
 }
 
+/*Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :*/
+
+class KataUnusualFive
+{
+    public static int UnusualFive() => "Hello".Length;
+}
+
 /*In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
 
 For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
