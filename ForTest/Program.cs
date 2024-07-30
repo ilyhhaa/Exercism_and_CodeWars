6f2fee8cd26c9205e38c61231db644ae3e9e4b57
Ilya <ilyhhaa@gmail.com>
2024-07-30 21:38:34 +0300

    In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.  For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.  Your function will be tested with pre-made examples as well as random ones.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -8,6 +8,19 @@ internal class Program
     }
 }
 
+/*In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+Your function will be tested with pre-made examples as well as random ones.*/
+
+public class Kata
+{
+  public static int FindDifference(int[] a, int[] b) =>
+    Math.Abs(a.Aggregate(1, (acc, val) => acc * val) - b.Aggregate(1, (acc, val) => acc * val));
+}
+
+
 /*You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
 
 Examples:
