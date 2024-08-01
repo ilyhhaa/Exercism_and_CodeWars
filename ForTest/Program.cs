70763947b8e28117a1ef602cd3a363d180e85d48
Ilya <ilyhhaa@gmail.com>
2024-08-01 21:19:37 +0300

    Ok,It's was hardOne

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -7,6 +7,75 @@ internal class Program
 
     }
 }
+
+/*Implement a pseudo-encryption algorithm which given a string S and an integer N concatenates all the odd-indexed characters of S with all the even-indexed characters of S, this process should be repeated N times.
+
+Examples:
+
+encrypt("012345", 1)  =>  "135024"
+encrypt("012345", 2)  =>  "135024"  ->  "304152"
+encrypt("012345", 3)  =>  "135024"  ->  "304152"  ->  "012345"
+
+encrypt("01234", 1)  =>  "13024"
+encrypt("01234", 2)  =>  "13024"  ->  "32104"
+encrypt("01234", 3)  =>  "13024"  ->  "32104"  ->  "20314"
+Together with the encryption function, you should also implement a decryption function which reverses the process.
+
+If the string S is an empty value or the integer N is not positive, return the first argument without changes.*/
+
+public class Kata
+{
+  public static string Encrypt(string text, int n)
+{
+    if (string.IsNullOrEmpty(text) || n <= 0)
+        return text;
+
+    for (int i = 0; i < n; i++)
+    {
+        var oddChars = new StringBuilder();
+        var evenChars = new StringBuilder();
+
+        for (int j = 0; j < text.Length; j++)
+        {
+            if (j % 2 == 0)
+                evenChars.Append(text[j]);
+            else
+                oddChars.Append(text[j]);
+        }
+
+        text = oddChars.ToString() + evenChars.ToString();
+    }
+
+    return text;
+}
+
+public static string Decrypt(string encryptedText, int n)
+{
+    if (string.IsNullOrEmpty(encryptedText) || n <= 0)
+        return encryptedText;
+
+    int halfLength = encryptedText.Length / 2;
+
+    for (int i = 0; i < n; i++)
+    {
+        var decrypted = new StringBuilder();
+
+        for (int j = 0; j < halfLength; j++)
+        {
+            decrypted.Append(encryptedText[halfLength + j]);
+            if (j < halfLength || encryptedText.Length % 2 == 0)
+                decrypted.Append(encryptedText[j]);
+        }
+
+        if (encryptedText.Length % 2 != 0)
+            decrypted.Append(encryptedText[encryptedText.Length - 1]);
+
+        encryptedText = decrypted.ToString();
+    }
+
+    return encryptedText;
+}
+}
 /*Complete the function that receives as input a string, and produces outputs according to the following table:
 
 Input	Output
