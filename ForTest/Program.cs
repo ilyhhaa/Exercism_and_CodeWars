ccd95b186cdb475b6472ab096f2c60e92b295326
Ilya <ilyhhaa@gmail.com>
2024-08-02 20:11:44 +0300

    Your function takes two arguments:  current father's age (years) current age of his son (years) Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -8,6 +8,21 @@ internal class Program
     }
 }
 
+/*Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+*/
+
+public class TwiceAsOldSolution
+  {
+    public static int TwiceAsOld(int dadYears, int sonYears)
+    {
+      return Math.Abs(dadYears - 2 * sonYears);
+    }
+  }
+
 /*Implement a pseudo-encryption algorithm which given a string S and an integer N concatenates all the odd-indexed characters of S with all the even-indexed characters of S, this process should be repeated N times.
 
 Examples:
