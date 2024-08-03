b47204477820d98f942cf5c3c5aefa4dd32db75b
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-08-03 23:20:04 +0300

    Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
    Examples (input -> output)
    '! !'                 -> '! !'
    '123456789'           -> ''
    'This looks5 grea8t!' -> 'This looks great!'
    Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers.
    Your program will take in a string and clean out all numeric characters, and return a string with spacing and special
    characters ~#$%^&!@*():;"'.,? all intact.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -6,7 +6,26 @@ internal class Program
     {
 
     }
+
 }
+/*Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+
+Examples (input -> output)
+'! !'                 -> '! !'
+'123456789'           -> ''
+'This looks5 grea8t!' -> 'This looks great!'
+Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers.
+Your program will take in a string and clean out all numeric characters, and return a string with spacing and special
+characters ~#$%^&!@*():;"'.,? all intact.*/
+
+public class KataStringClean
+{
+    public static string StringClean(string s)
+    {
+        return new string(s.Where(c => !char.IsDigit(c)).ToArray());
+    }
+}
+
 /*Write a function named setAlarm/set_alarm/set-alarm/setalarm (depending on language) which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
 
 The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
