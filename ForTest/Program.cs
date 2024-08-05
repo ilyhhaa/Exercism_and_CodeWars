806635c2b7d9ddc91737899e83c7397dad9d1eca
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-08-05 23:52:58 +0300

    Fellow code warrior, we need your help! We seem to have lost one of our sequence elements, and we need your help to retrieve it!
    Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
    
    Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
    
    Examples:
    [0, 5, 1, 3, 2, 9, 7, 6, 4] --> 8
    [9, 2, 4, 5, 7, 0, 8, 6, 1] --> 3

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -8,9 +8,36 @@ internal class Program
     }
 
 }
+
+/*Fellow code warrior, we need your help! We seem to have lost one of our sequence elements, and we need your help to retrieve it!
+
+Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
+
+Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
+
+Examples:
+[0, 5, 1, 3, 2, 9, 7, 6, 4] --> 8
+[9, 2, 4, 5, 7, 0, 8, 6, 1] --> 3*/
+
+public static class KataGetMissing
+{
+    public static int GetMissingElement(int[] superImportantArray)
+    {
+        int expectedSum = 45;
+        int actualSum = 0;
+
+        foreach (int num in superImportantArray)
+        {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+}
+
 /*Complete the function which converts a binary number (given as a string) to a decimal number.*/
 
-public  class BinToDec 
+public class BinToDec 
 {
     public int binToDec(string s)
     {
