7b9ae1f77883bf54259a52c47c886679f7c8890d
Ilya <ilyhhaa@gmail.com>
2024-08-06 20:57:37 +0300

    Update Program.cs

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,25 @@ internal class Program
 
 }
 
+/*The purpose of this kata is to work out just how many bottles of duty free whiskey you would have to buy such that the savings over the normal high street price would effectively cover the cost of your holiday.
+
+You will be given the high street price (normPrice, in £ (Pounds)), the duty free discount (discount, in percent) and the cost of the holiday (in £).
+
+For example, if a bottle costs £10 normally and the duty free discount is 10%, you would save £1 per bottle. If your holiday will cost £500, you would have to purchase 500 bottles to save £500, so the answer you return should be 500.
+
+Another example: if a bottle costs £12 normally and the duty free discount is 50%, you would save £6 per bottle. If your holiday will cost £1000, you would have to purchase 166.66 bottles to save £1000, so your answer should be 166 bottles.
+
+All inputs will be integers. Please return an integer. Round down.*/
+
+public class KataDutyFree
+{
+  public static int DutyFree(int normPrice, int Discount, int hol)
+  {
+    double savingsPerBottle = normPrice * (Discount / 100.0);
+    int bottlesNeeded = (int)(hol / savingsPerBottle);
+    return bottlesNeeded;
+  }
+}
 /*Fellow code warrior, we need your help! We seem to have lost one of our sequence elements, and we need your help to retrieve it!
 
 Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
