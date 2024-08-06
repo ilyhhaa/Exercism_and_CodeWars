343d745632ae87b60d2618ec9282ada92194fcac
Ilya <ilyhhaa@gmail.com>
2024-08-06 21:18:26 +0300

    return 5050 - nums.Sum();

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,7 @@ internal class Program
 }
 
 
+
 /*The following was a question that I received during a technical interview for an entry level software developer position. I thought I'd post it here so that everyone could give it a go:
 
 You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is missing. Write a function to find and return this number. What are the time and space complexities of your solution?*/
@@ -22,6 +23,9 @@ public static class KataMissingNo
     int expectedSum = n * (n + 1) / 2;
     int actualSum = nums.Sum();
     return expectedSum - actualSum;
+
+
+        //return 5050 - nums.Sum();
     }
 }
 
