3069f516bba57b6045e7812dc17605ab791468da
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-08-14 10:26:39 +0300

    You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
    Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
    
    []                                -->  "no one likes this"
    ["Peter"]                         -->  "Peter likes this"
    ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
    ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
    ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
    Note: For 4 or more names, the number in "and 2 others" simply increases.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,39 @@ internal class Program
 
 }
 
+/*You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+Note: For 4 or more names, the number in "and 2 others" simply increases.*/
+
+public static class KataLikes
+{
+    public static string Likes(string[] name)
+    {
+        int count = name.Length;
+
+        switch (count)
+        {
+            case 0:
+                return "no one likes this";
+            case 1:
+                return $"{name[0]} likes this";
+            case 2:
+                return $"{name[0]} and {name[1]} like this";
+            case 3:
+                return $"{name[0]}, {name[1]} and {name[2]} like this";
+            default:
+                return $"{name[0]}, {name[1]} and {count - 2} others like this";
+        }
+    }
+}
+
 /*Your task is to find the nearest square number, nearest_sq(n) or nearestSq(n), of a positive integer n.
 
 For example, if n = 111, then nearest\_sq(n) (nearestSq(n)) equals 121, since 111 is closer to 121, the square of 11, than 100, the square of 10.
