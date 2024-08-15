5b103b29b7d931147bb50fe4741e2bb799c1ca39
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-08-15 22:50:41 +0300

    Write function bmi that calculates body mass index (bmi = weight / height2).
    if bmi <= 18.5 return "Underweight"
    
    if bmi <= 25.0 return "Normal"
    
    if bmi <= 30.0 return "Overweight"
    
    if bmi > 30 return "Obese"

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,9 +9,37 @@ internal class Program
 
 }
 
+/*Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"*/
+
+public class KataBmi
+{
+    public static string Bmi(double weight, double height)
+    {
+        double bmi = weight / (height * height);
+
+        if (bmi <= 18.5)
+            return "Underweight";
+        else if (bmi <= 25.0)
+            return "Normal";
+        else if (bmi <= 30.0)
+            return "Overweight";
+        else
+            return "Obese";
+    }
+}
+
+
 /*fastest as u can you need to double the integer and return it.*/
 
-public static class Kata
+public static class KataDoubleInt
 {
     public static int DoubleInteger(int n)
     {
