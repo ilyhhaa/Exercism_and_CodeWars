91d26a52fde565ff1f84c3190bba297ac9f1ef88
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-08-16 11:39:22 +0300

    This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
    Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to return an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
    
    For example:
    
    derive(7, 8)
    In this case, the function should multiply 7 and 8, and then subtract 1 from 8. It should output "56x^7", the first number 56 being the product of the two numbers, and the second number being the exponent minus 1.
    
    derive(7, 8) --> this should output "56x^7"
    derive(5, 9) --> this should output "45x^8"

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,27 @@ internal class Program
 
 }
 
+/*This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
+
+Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to return an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+
+For example:
+
+derive(7, 8)
+In this case, the function should multiply 7 and 8, and then subtract 1 from 8. It should output "56x^7", the first number 56 being the product of the two numbers, and the second number being the exponent minus 1.
+
+derive(7, 8) --> this should output "56x^7" 
+derive(5, 9) --> this should output "45x^8" */
+
+
+public class Kata
+{
+    public static string Derive(double coefficient, double exponent)
+    {
+        return new string($"{coefficient * exponent}x^{exponent - 1}");
+    }
+}
+
 /*Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
 
 */
