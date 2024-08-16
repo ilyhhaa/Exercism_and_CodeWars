bd090492c872d8cd0f634ffebf6c8f302305adff
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-08-16 12:22:34 +0300

    John has invited some friends. His list is:
    s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
    Could you make a program that
    
    makes this string uppercase
    gives it sorted in alphabetical order by last name.
    When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
    
    So the result of function meeting(s) will be:
    
    "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
    It can happen that in two distinct families with the same family name two people have the same first name too.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,42 @@ internal class Program
 
 }
 
+/**/
+
+/*John has invited some friends. His list is:
+
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+Could you make a program that
+
+makes this string uppercase
+gives it sorted in alphabetical order by last name.
+When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+So the result of function meeting(s) will be:
+
+"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+It can happen that in two distinct families with the same family name two people have the same first name too.*/
+
+public class JohnMeeting
+{
+    public static string Meeting(string s)
+    {
+        var names = s.Split(';');
+        var formattedNames = new List<string>();
+
+        foreach (var name in names)
+        {
+            var parts = name.Split(':');
+            var firstName = parts[0].ToUpper();
+            var lastName = parts[1].ToUpper();
+
+            formattedNames.Add($"({lastName}, {firstName})");
+        }
+        formattedNames.Sort();
+        return string.Join("", formattedNames);
+    }
+}
+
 /*This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
 
 Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to return an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
