1e725aeedef3d2d7754f86ba73fe178dec31fd55
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-08-22 14:41:42 +0300

    Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
    Please use the following function names:
    
    addition = Add
    
    multiply = Multiply
    
    division = Divide
    
    modulus = Mod
    
    exponential = Exponent
    
    subtraction = Subt
    
    Note: All funcitons can return int and all will recieve 2 integers.
    
    Note: All math operations will be: a (operation) b

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -8,7 +8,41 @@ internal class Program
     {
 
     }
+    /*Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
 
+Please use the following function names:
+
+addition = Add
+
+multiply = Multiply
+
+division = Divide
+
+modulus = Mod
+
+exponential = Exponent
+
+subtraction = Subt
+
+Note: All funcitons can return int and all will recieve 2 integers.
+
+Note: All math operations will be: a (operation) b*/
+
+    public static class KataFunctions
+    {
+        public static int Add(int a, int b) => a + b;
+
+        public static int Subt(int a, int b) => a - b;
+
+        public static int Multiply(int a, int b) => a * b;
+
+        public static int Divide(int a, int b) => a / b;
+
+        public static int Mod(int a, int b) => a % b;
+
+        public static int Exponent(int a, int b) => Convert.ToInt32(Math.Pow(a, b));
+
+    }
 
     /*Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
 
