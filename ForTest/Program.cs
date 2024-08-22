87fcc0c3e2b0a96ec1e06ec3267c4dd0c1cd65d0
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-08-22 13:44:18 +0300

    I have a cat and a dog.
    I got them at the same time as kitten/puppy. That was humanYears years ago.
    
    Return their respective ages now as [humanYears,catYears,dogYears]
    
    NOTES:
    
    humanYears >= 1
    humanYears are whole numbers only
    Cat Years
    15 cat years for first year
    +9 cat years for second year
    +4 cat years for each year after that
    Dog Years
    15 dog years for first year
    +9 dog years for second year
    +5 dog years for each year after that

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -8,6 +8,37 @@ internal class Program
     {
 
     }
+    /*I have a cat and a dog.
+
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+NOTES:
+
+humanYears >= 1
+humanYears are whole numbers only
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that*/
+
+    public class Dinglemouse
+    {
+
+        public static int[] humanYearsCatYearsDogYears(int humanYears)
+        {
+            int catYear = 15 + (humanYears >= 2 ? 9 + 4 * (humanYears - 2) : 0);
+            int dogYear = 15 + (humanYears >= 2 ? 9 + 5 * (humanYears - 2) : 0);
+
+            return new int[] { humanYears, catYear, dogYear };
+        }
+
+    }
 
     /*Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
 
