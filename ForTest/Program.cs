db3827037fd08e5c4557ac8f5cbc175cc4c2a6c5
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-08-27 17:52:00 +0300

    Input: Array of elements
    ["h","o","l","a"]
    
    Output: String with comma delimited elements of the array in th same order.
    
    "h,o,l,a"
    
    Note: if this seems too simple for you try the next level
    
    Note2: the input data can be: boolean array, array of objects, array of string arrays, array of number arrays... 😕

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,27 @@ internal class Program
 
     }
 
+    /*Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+
+Note: if this seems too simple for you try the next level
+
+Note2: the input data can be: boolean array, array of objects, array of string arrays, array of number arrays... 😕*/
+
+    public class KataPrintArray
+    {
+        public static string PrintArray(object[] array)
+        {
+            return string.Join(",", array.Select(a => a.GetType().IsArray ? string.Join(",", (object[])a) : a));
+        }
+    }
+
+
     /*You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
 
 Your function will accept three arguments:
