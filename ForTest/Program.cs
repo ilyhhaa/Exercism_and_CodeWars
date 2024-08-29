85741b4a818662494ce5d9441ba10ef1d543ea19
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-08-29 08:44:17 +0300

    I assume most of you are familiar with the ancient legend of the rice (but I see wikipedia suggests wheat, for some reason) problem, but a quick recap for you: a young man asks as a compensation only 1 grain of rice for the first square, 2 grains for the second, 4 for the third, 8 for the fourth and so on, always doubling the previous.
    Your task is pretty straightforward (but not necessarily easy): given an amount of grains, you need to return up to which square of the chessboard one should count in order to get at least as many.
    
    As usual, a few examples might be way better than thousands of words from me:
    
    0 grains need 0 cells
    1 grain needs 1 cell
    2 grains need 2 cells
    3 grains need 2 cells
    4 grains need 3 cells
    and etc

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,25 @@ internal class Program
 
     }
 
+    /*I assume most of you are familiar with the ancient legend of the rice (but I see wikipedia suggests wheat, for some reason) problem, but a quick recap for you: a young man asks as a compensation only 1 grain of rice for the first square, 2 grains for the second, 4 for the third, 8 for the fourth and so on, always doubling the previous.
+
+Your task is pretty straightforward (but not necessarily easy): given an amount of grains, you need to return up to which square of the chessboard one should count in order to get at least as many.
+
+As usual, a few examples might be way better than thousands of words from me:
+
+0 grains need 0 cells
+1 grain needs 1 cell
+2 grains need 2 cells
+3 grains need 2 cells
+4 grains need 3 cells
+and etc.*/
+
+    class Kata
+    {
+        public static int SquaresNeeded(long grains)
+          => grains > 0 ? 1 + SquaresNeeded(grains / 2) : 0;
+    }
+
     /*Write a function that takes a positive integer n, sums all the cubed values from 1 to n (inclusive), and returns that sum.
 
 Assume that the input n will always be a positive integer.
