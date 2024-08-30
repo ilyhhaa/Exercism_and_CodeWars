878e35340f2817b716fe8f4c7e9b35597e283196
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-08-30 17:45:49 +0300

    There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
    number of pillars (≥ 1);
    distance between pillars (10 - 30 meters);
    width of the pillar (10 - 50 centimeters).
    Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,10 +9,25 @@ internal class Program
 
     }
 
+    /*There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+
+number of pillars (≥ 1);
+distance between pillars (10 - 30 meters);
+width of the pillar (10 - 50 centimeters).
+Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+
+*/
+
+    public static class KataPillars
+    {
+        public static int Pillars(int numPill, int dist, int width)
+        {
+            return numPill > 1 ? (numPill - 1) * dist * 100 + width * (numPill - 2) : 0;
+        }
+    }
 
 
-
-    public class Kata
+    public class KataMakeUpperCase
     {
         public static string MakeUpperCase(string str)
         {
