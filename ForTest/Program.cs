9f40fc6e526ac09222b5003293ff078b635cb15a
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-08-30 17:21:59 +0300

    Str.ToUpper();

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,19 @@ internal class Program
 
     }
 
+
+
+
+    public class Kata
+    {
+        public static string MakeUpperCase(string str)
+        {
+            return str.ToUpper();
+        }
+
+    }
+
+
     /*Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
 
 Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
