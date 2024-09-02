1cdeeed66d0a90eda4d61cf88a43e19d8ddb2328
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-02 09:22:27 +0300

    Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
    Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
    
    Create a function which translates a given DNA string into RNA.
    
    For example:
    
    "GCAT"  =>  "GCAU"
    The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -26,6 +26,24 @@ public class KataRemove
     }
 }
 
+/*Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.*/
+public class Converter
+{
+    public string dnaToRna(string dna)
+    {
+        return dna.Replace('T', 'U');
+    }
+}
+
 /*Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
 
 Notes
@@ -61,4 +79,5 @@ public class KataAdjacentElementsProduct
         return maxProduct;
 
     }
+    // return array.Skip(1).Select((x,i) => x * array[i]).Max();
 }
