0af0c10a00c6b737f00de3faf418d396a660ec6d
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-02 09:46:35 +0300

    Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
    Use conditionals to return the proper message:
    
    case return
    name equals owner 'Hello boss'
    otherwise 'Hello guest'

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -8,6 +8,22 @@ internal class Program
 
     }
 }
+/*Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'*/
+
+public class Kata
+{
+    public static string Greet(string name, string owner)
+    {
+        return (name == owner) ? "Hello boss" : "Hello guest";
+    }
+}
+
 /*Remove all exclamation marks from the end of sentence.
 
 Examples
