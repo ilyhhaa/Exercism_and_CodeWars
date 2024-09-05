cc19ac1a906d6e6a23080f7f588a9f3076bffcf5
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-05 23:57:08 +0300

    /*Write a function that returns a string in which firstname is swapped with last name.
    Example(Input --> Output)
    
    "john McClane" --> "McClane john"*/

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,21 @@ internal class Program
     }
 }
 
+/*Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"*/
+
+public class KataNameShuffler
+{
+    public static string NameShuffler(string str)
+    {
+        string[] names = str.Split(' ');
+        return names[1] + " " + names[0];
+    }
+}
+
 /*Christmas is coming and many people dreamed of having a ride with Santa's sleigh. But, of course, only Santa himself is allowed to use this wonderful transportation. And in order to make sure, that only he can board the sleigh, there's an authentication mechanism.
 
 Your task is to implement the authenticate() method of the sleigh, which takes the name of the person, who wants to board the sleigh and a secret password. If, and only if, the name equals "Santa Claus" and the password is "Ho Ho Ho!" (yes, even Santa has a secret password with uppercase and lowercase letters and special characters :D), the return value must be true. Otherwise it should return false.*/
