7ea7d36bb315346928dc8aca631230edcf6a0e5c
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-05 13:26:25 +0300

    Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
    */

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -8,6 +8,24 @@ internal class Program
 
     }
 }
+
+/*Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+*/
+
+public static class KataCountSheep
+{
+    public static string CountSheep(int n)
+    {
+        string result = "";
+        for (int i = 1; i <= n; i++)
+        {
+            result += $"{i} sheep...";
+        }
+        return result;
+    }
+}
+
 /*Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
 
 Examples:
