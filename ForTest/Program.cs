4c73b23c92ee1710b1e97f606bf1acfe4f9083b8
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-06 08:56:39 +0300

    Write a function that will check if two given characters are the same case.
    If either of the characters is not a letter, return -1
    If both characters are the same case, return 1
    If both characters are letters, but not the same case, return 0
    Examples
    'a' and 'g' returns 1
    
    'A' and 'C' returns 1
    
    'b' and 'G' returns 0
    
    'B' and 'g' returns 0
    
    '0' and '?' returns -1

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -8,6 +8,36 @@ internal class Program
 
     }
 }
+/*Write a function that will check if two given characters are the same case.
+
+If either of the characters is not a letter, return -1
+If both characters are the same case, return 1
+If both characters are letters, but not the same case, return 0
+Examples
+'a' and 'g' returns 1
+
+'A' and 'C' returns 1
+
+'b' and 'G' returns 0
+
+'B' and 'g' returns 0
+
+'0' and '?' returns -1*/
+public class KataSameCase
+{
+    public static int SameCase(char a, char b)
+    {
+
+        if (!char.IsLetter(a) || !char.IsLetter(b))
+        {
+            return -1;
+        }
+
+        return char.IsUpper(a) == char.IsUpper(b) ? 1 : 0;
+    }
+}
+
+
 /*You get any card as an argument. Your task is to return the suit of this card (in lowercase).
 Our deck (is preloaded):
 string[] Deck =
