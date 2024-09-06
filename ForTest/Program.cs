2554d6efcf0d7fd49cd3e9913559d77370718437
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-06 23:55:38 +0300

    Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
    Examples
    Input: [1, 5.2, 4, 0, -1]
    Output: 9.2
    
    Input: []
    Output: 0
    
    Input: [-2.398]
    Output: -2.398
    
    Assumptions
    You can assume that you are only given numbers.
    You cannot assume the size of the array.
    You can assume that you do get an array and if the array is empty, return 0.
    What We're Testing
    We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
    Advanced users may find this extremely easy and can easily write this in one line.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -8,6 +8,42 @@ internal class Program
 
     }
 }
+
+/*Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+Examples
+Input: [1, 5.2, 4, 0, -1]
+Output: 9.2
+
+Input: []
+Output: 0
+
+Input: [-2.398]
+Output: -2.398
+
+Assumptions
+You can assume that you are only given numbers.
+You cannot assume the size of the array.
+You can assume that you do get an array and if the array is empty, return 0.
+What We're Testing
+We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
+Advanced users may find this extremely easy and can easily write this in one line.*/
+
+public class KataSumArray
+{
+    public static double SumArray(double[] array)
+    {
+        double sum = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+
+        return sum;
+    }
+}
+
 /*Write a function that will check if two given characters are the same case.
 
 If either of the characters is not a letter, return -1
