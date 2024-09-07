fee4ae9713fc230de7c83d2d2d3d9ba842cc6f87
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-07 10:14:02 +0300

    Create a function that returns the CSV representation of a two-dimensional numeric array.
    Example:
    
    input:
       [[ 0, 1, 2, 3, 4 ],
        [ 10,11,12,13,14 ],
        [ 20,21,22,23,24 ],
        [ 30,31,32,33,34 ]]
    
    output:
         '0,1,2,3,4\n'
        +'10,11,12,13,14\n'
        +'20,21,22,23,24\n'
        +'30,31,32,33,34'
    Array's length > 2.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Text;
 using System.Text.RegularExpressions;
 
 internal class Program
@@ -6,6 +7,36 @@ internal class Program
     private static void Main(string[] args)
     {
 
+    }
+}
+/*Create a function that returns the CSV representation of a two-dimensional numeric array.
+
+Example:
+
+input:
+   [[ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]] 
+    
+output:
+     '0,1,2,3,4\n'
+    +'10,11,12,13,14\n'
+    +'20,21,22,23,24\n'
+    +'30,31,32,33,34'
+Array's length > 2.*/
+public static class KataToCsvText
+{
+    public static string ToCsvText(int[][] array)
+    {
+        StringBuilder csvText = new StringBuilder();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            csvText.AppendLine(string.Join(",", array[i]));
+        }
+
+        return csvText.ToString().TrimEnd();
     }
 }
 
