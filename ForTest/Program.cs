8c692378f5fbcb7279894547513e3dca686c82c8
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-10 16:22:24 +0300

    Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
    Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
    
    solve(["abode","ABc","xyzD"]) = [4, 3, 1]
    See test cases for more examples.
    
    Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
    
    Good luck!

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,37 @@ internal class Program
 
     }
 }
+
+/*Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+
+Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+
+solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+See test cases for more examples.
+
+Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
+
+Good luck!*/
+
+public static class Kata
+{
+    public static List<int> Solve(List<string> arr)
+    {
+        return arr.Select(word =>
+        {
+            int count = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (char.ToLower(word[i]) == 'a' + i)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }).ToList();
+    }
+}
+
 /*Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
 
 Example:
