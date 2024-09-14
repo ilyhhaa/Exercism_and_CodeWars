885f0713c3d2ced4984549cf086dc6384435cc2e
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-14 21:03:15 +0300

    /*Template Strings Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings Task Your task is to return the correct string using the Template String Feature. Input Two Strings, no validation is needed. Output You must output a string containing the two strings with the word ```' are '```*/

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,25 @@ internal class Program
 
     }
 }
+
+/*Template Strings
+Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+Task
+Your task is to return the correct string using the Template String Feature.
+Input
+Two Strings, no validation is needed.
+Output
+You must output a string containing the two strings with the word ```' are '```*/
+
+public class Templates
+{
+    public static string TempleStrings(string obj, string feature)
+    {
+        return $"{obj} are {feature}";
+    }
+}
+
+
 /*Classy Classes
 Basic Classes, this kata is mainly aimed at the new JS ES6 Update introducing classes
 
