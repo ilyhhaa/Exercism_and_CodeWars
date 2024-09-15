0ff03f871b643f9bcafe54d49a26eb75a3a16ee0
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-15 11:23:00 +0300

    You can print your name on a billboard ad. Find out how much it will cost you. Each character has a default price of £30, but that can be different if you are given 2 parameters instead of 1 (allways 2 for Java).
    You can not use multiplier "*" operator.
    
    If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a character)

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,28 @@ internal class Program
 
     }
 }
+/*You can print your name on a billboard ad. Find out how much it will cost you. Each character has a default price of £30, but that can be different if you are given 2 parameters instead of 1 (allways 2 for Java).
+
+You can not use multiplier "*" operator.
+
+If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a character).*/
+
+
+public class KataBillboard
+{
+    public static double Billboard(string name, double price = 30)
+    {
+        double result = 0;
+
+        for (int i = 0; i < name.Length; i++)
+        {
+            result += price;
+        }
+
+        return result;
+    }
+}
+
 
 /*Create a function close_compare that accepts 3 parameters: a, b, and an optional margin. The function should return whether a is lower than, close to, or higher than b.
 
