645107b115f4bc48c9b6ddb649cdaeb78412be77
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-16 23:54:23 +0300

    You have to write a function that describe Leo:
    def leo(oscar):
      pass
    if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
    if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
    if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
    if it was over 88 you should return "Leo got one already!"

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,39 @@ internal class Program
     }
 }
 
+
+/*You have to write a function that describe Leo:
+
+def leo(oscar):
+  pass
+if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
+if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
+if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
+if it was over 88 you should return "Leo got one already!"*/
+
+public static class KataLEO
+{
+    public static string Leo(int oscar)
+    {
+        switch (oscar)
+        {
+            case 88:
+                return "Leo finally won the oscar! Leo is happy";
+            case 86:
+                return "Not even for Wolf of wallstreet?!";
+            default:
+                if (oscar < 88)
+                {
+                    return "When will you give Leo an Oscar?";
+                }
+                else
+                {
+                    return "Leo got one already!";
+                }
+        }
+    }
+}
+
 /*The male gametes or sperm cells in humans and other mammals are heterogametic and contain one of two types of sex chromosomes. They are either X or Y. The female gametes or eggs however, contain only the X sex chromosome and are homogametic.
 
 The sperm cell determines the sex of an individual in this case. If a sperm cell containing an X chromosome fertilizes an egg, the resulting zygote will be XX or female. If the sperm cell contains a Y chromosome, then the resulting zygote will be XY or male.
