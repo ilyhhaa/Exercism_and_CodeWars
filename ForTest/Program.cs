dc540eade65a3685148976238612b2fe5d9b2018
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-17 09:23:10 +0300

    Finish the uefaEuro2016() function so it return string just like in the examples below:
    uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
    uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
    uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,22 @@ internal class Program
     }
 }
 
+/*Finish the uefaEuro2016() function so it return string just like in the examples below:
+
+uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."*/
+
+public class KataUefaEuro2016
+{
+    public static string UefaEuro2016(string[] teams, int[] scores)
+    {
+        var result = scores[0] == scores[1] ? "teams played draw." :
+                     scores[0] > scores[1] ? $"{teams[0]} won!" : $"{teams[1]} won!";
+
+        return $"At match {teams[0]} - {teams[1]}, {result}";
+    }
+}
 
 /*You have to write a function that describe Leo:
 
