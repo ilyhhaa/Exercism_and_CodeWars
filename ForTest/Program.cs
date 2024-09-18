b1073e8968db29bf74629bea1bab457ea5428350
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-18 16:31:52 +0300

    GetGrade()

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,37 @@ internal class Program
     }
 }
 
+/*Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+Numerical Score	Letter Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.*/
+
+
+public class KataGetGrade
+{
+    public static char GetGrade(int s1, int s2, int s3)
+    {
+        double average = (s1 + s2 + s3) / 3.0;
+
+        return average switch
+        {
+            >= 90 => 'A',
+            >= 80 => 'B',
+            >= 70 => 'C',
+            >= 60 => 'D',
+            _ => 'F'
+        };
+  
+     }  
+}
+
+
+
 /*The objective of Duck, duck, goose is to walk in a circle, tapping on each player's head until one is chosen.
 
 Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
