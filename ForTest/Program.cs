78b4d4a24723a622dba3bc495b509f9309021306
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-20 10:03:09 +0300

    We want to generate a function that computes the series starting from 0 and ending until the given number.
    Example:
    Input:
    
    > 6
    Output:
    
    0+1+2+3+4+5+6 = 21
    
    Input:
    
    > -15
    Output:
    
    -15<0
    
    Input:
    
    > 0
    Output:
    
    0=0

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,43 @@ internal class Program
 
     }
 }
+/*We want to generate a function that computes the series starting from 0 and ending until the given number.
+
+Example:
+Input:
+
+> 6
+Output:
+
+0+1+2+3+4+5+6 = 21
+
+Input:
+
+> -15
+Output:
+
+-15<0
+
+Input:
+
+> 0
+Output:
+
+0=0*/
+
+public class SequenceSum
+{
+    public static string ShowSequence(int n)
+    {
+        if (n < 0) return $"{n}<0";
+        if (n == 0) return "0=0";
+
+        IEnumerable<int> sequence = Enumerable.Range(0, n + 1);
+
+        return $"{String.Join("+", sequence)} = {sequence.Sum()}";
+    }
+}
+
 
 /*You might know some pretty large perfect squares. But what about the NEXT one?
 
