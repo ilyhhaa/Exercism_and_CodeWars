42d3178bbe508054b8ab492b2a4fd06a54842416
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-21 09:53:35 +0300

    This is a beginner friendly kata especially for UFC/MMA fans.
    It's a fight between the two legends: Conor McGregor vs George Saint Pierre in Madison Square Garden. Only one fighter will remain standing, and after the fight in an interview with Joe Rogan the winner will make his legendary statement. It's your job to return the right statement depending on the winner!
    
    If the winner is George Saint Pierre he will obviously say:
    
    "I am not impressed by your performance."
    If the winner is Conor McGregor he will most undoubtedly say:
    
    "I'd like to take this chance to apologize.. To absolutely NOBODY!"
    Good Luck!
    
    Note
    The given name may varies in casing, eg., it can be "George Saint Pierre" or "geOrGe saiNT pieRRE". Your solution should treat both as the same thing (case-insensitive).

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,27 @@ internal class Program
 
     }
 }
+/*This is a beginner friendly kata especially for UFC/MMA fans.
+
+It's a fight between the two legends: Conor McGregor vs George Saint Pierre in Madison Square Garden. Only one fighter will remain standing, and after the fight in an interview with Joe Rogan the winner will make his legendary statement. It's your job to return the right statement depending on the winner!
+
+If the winner is George Saint Pierre he will obviously say:
+
+"I am not impressed by your performance."
+If the winner is Conor McGregor he will most undoubtedly say:
+
+"I'd like to take this chance to apologize.. To absolutely NOBODY!"
+Good Luck!
+
+Note
+The given name may varies in casing, eg., it can be "George Saint Pierre" or "geOrGe saiNT pieRRE". Your solution should treat both as the same thing (case-insensitive).*/
+public static class KataQuote
+{
+    public static string Quote(string fighter)
+    {
+        return fighter.ToLower() == "conor mcgregor" ? @"I'd like to take this chance to apologize.. To absolutely NOBODY!" : @"I am not impressed by your performance.";
+    }
+}
 
 /*Filter the number
 Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
