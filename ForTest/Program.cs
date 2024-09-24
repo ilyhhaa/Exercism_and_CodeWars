12c6770f6730099a7c767918ec30f51fb4e7f466
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-24 19:12:19 +0300

    You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
    Find max(abs(length(x) − length(y)))
    
    If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
    
    Example:
    a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
    a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
    mxdiflg(a1, a2) --> 13
    Bash note:
    input : 2 strings with substrings separated by ,
    output: number as a string

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,8 +10,42 @@ internal class Program
     }
 }
 
+/*
+ You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
 
+Find max(abs(length(x) − length(y)))
 
+If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+Example:
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+mxdiflg(a1, a2) --> 13
+Bash note:
+input : 2 strings with substrings separated by ,
+output: number as a string
+ */
+public class MaxDiffLength
+{
+    public static int Mxdiflg(string[] a1, string[] a2)
+    {
+        switch (a1.Length == 0 || a2.Length == 0)
+        {
+            case true:
+                return -1;
+            case false:
+                int maxLenA1 = a1.Max(s => s.Length);
+                int minLenA1 = a1.Min(s => s.Length);
+                int maxLenA2 = a2.Max(s => s.Length);
+                int minLenA2 = a2.Min(s => s.Length);
+
+                int maxDiff1 = Math.Abs(maxLenA1 - minLenA2);
+                int maxDiff2 = Math.Abs(maxLenA2 - minLenA1);
+
+                return Math.Max(maxDiff1, maxDiff2);
+        }
+    }
+}
 /*
 In this first kata in the series, you need to define a Hero class to be used in a terminal game. The hero should have the following attributes:
 
