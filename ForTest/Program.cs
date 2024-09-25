29446c83301ce31e51d7eda872fe4d1e41c6bfdc
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-25 09:55:45 +0300

    All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
    Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
    
    Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,7 +10,19 @@ internal class Program
     }
 }
 
+/*All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
 
+Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.*/
+
+public class KataFeast
+{
+    public static bool Feast(string beast, string dish)
+    {
+        return beast[0] == dish[0] && beast[beast.Length - 1] == dish[dish.Length - 1];
+    }
+}
 
 /*
  Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
@@ -1019,7 +1031,7 @@ Examples
 [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
 [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3*/
 
-public class Kata
+public class KataHighestRank
 {
     public static int HighestRank(int[] arr)
     {
