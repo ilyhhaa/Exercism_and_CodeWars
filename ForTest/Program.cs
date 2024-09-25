4e081ae23f9c544375956146cd7ef894c3367f35
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-25 09:35:51 +0300

    Time to test your basic knowledge in functions! Return the odds from a list:
    [1, 2, 3, 4, 5]  -->  [1, 3, 5]
    [2, 4, 6]        -->  []

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,17 @@ internal class Program
     }
 }
 
+/*Time to test your basic knowledge in functions! Return the odds from a list:
+
+[1, 2, 3, 4, 5]  -->  [1, 3, 5]
+[2, 4, 6]        -->  []*/
+
+public static class Kata
+{
+    public static List<int> Odds(List<int> values) => values.Where(v => v % 2 != 0).ToList();
+
+}
+
 /*
 Find the number with the most digits.
 
