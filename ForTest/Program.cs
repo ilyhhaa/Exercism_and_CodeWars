63b57d4e44c56eba4ad35bc9c88e8100fed17084
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-26 09:17:12 +0300

    Task Given a number determine if it Automorphic or not .
    Warm-up (Highly recommended)
    Playing With Numbers Series
    Notes
    The number passed to the function is positive
    Input >> Output Examples
    autoMorphic (25) -->> return "Automorphic"
    Explanation:
    25 squared is 625 , Ends with the same number's digits which are 25 .
    
    autoMorphic (13) -->> return "Not!!"
    Explanation:
    13 squared is 169 , Not ending with the same number's digits which are 69 .
    
    autoMorphic (76) -->> return "Automorphic"
    Explanation:
    76 squared is 5776 , Ends with the same number's digits which are 76 .
    
    autoMorphic (225) -->> return "Not!!"
    Explanation:
    225 squared is 50625 , Not ending with the same number's digits which are 225 .
    
    autoMorphic (625) -->> return "Automorphic"
    Explanation:
    625 squared is 390625 , Ends with the same number's digits which are 625 .
    
    autoMorphic (1) -->> return "Automorphic"
    Explanation:
    1 squared is 1 , Ends with the same number's digits which are 1 .
    
    autoMorphic (6) -->> return "Automorphic"
    Explanation:
    6 squared is 36 , Ends with the same number's digits which are 6

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -11,6 +11,62 @@ internal class Program
 }
 
 /*
+ Task
+Given a number determine if it Automorphic or not .
+
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+The number passed to the function is positive
+Input >> Output Examples
+autoMorphic (25) -->> return "Automorphic" 
+Explanation:
+25 squared is 625 , Ends with the same number's digits which are 25 .
+
+autoMorphic (13) -->> return "Not!!"
+Explanation:
+13 squared is 169 , Not ending with the same number's digits which are 69 .
+
+autoMorphic (76) -->> return "Automorphic"
+Explanation:
+76 squared is 5776 , Ends with the same number's digits which are 76 .
+
+autoMorphic (225) -->> return "Not!!"
+Explanation:
+225 squared is 50625 , Not ending with the same number's digits which are 225 .
+
+autoMorphic (625) -->> return "Automorphic"
+Explanation:
+625 squared is 390625 , Ends with the same number's digits which are 625 .
+
+autoMorphic (1) -->> return "Automorphic"
+Explanation:
+1 squared is 1 , Ends with the same number's digits which are 1 .
+
+autoMorphic (6) -->> return "Automorphic"
+Explanation:
+6 squared is 36 , Ends with the same number's digits which are 6
+ */
+
+class KataAutomorphic
+{
+    public static string Automorphic(int n)
+    {
+        int square = n * n;
+        string nStr = n.ToString();
+        string squareStr = square.ToString();
+        if (squareStr.EndsWith(nStr))
+        {
+            return "Automorphic";
+        }
+        else
+        {
+            return "Not!!";
+        }
+    }
+}
+
+/*
  Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
 
 Examples
