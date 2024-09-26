cdfc0dbf49e47933871e85c2ee7ea1551ce55e79
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-26 09:14:43 +0300

    Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
    Examples
    "Hi!"     ---> "Hi!"
    "Hi!!!"   ---> "Hi!"
    "!Hi"     ---> "Hi!"
    "!Hi!"    ---> "Hi!"
    "Hi! Hi!" ---> "Hi Hi!"
    "Hi"      ---> "Hi!"

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,28 @@ internal class Program
     }
 }
 
+/*
+ Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+
+Examples
+"Hi!"     ---> "Hi!"
+"Hi!!!"   ---> "Hi!"
+"!Hi"     ---> "Hi!"
+"!Hi!"    ---> "Hi!"
+"Hi! Hi!" ---> "Hi Hi!"
+"Hi"      ---> "Hi!"
+ */
+
+public class KataRemoveSReplace
+{
+    public static string Remove(string s)
+    {
+        string result = s.Replace("!", "");
+        return result + "!";
+    }
+}
+
+
 /*All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
 
 Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
