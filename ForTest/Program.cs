9ff01f5a5efd25e638d434e121347f608c17fdaf
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-28 11:45:37 +0300

    I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
    Example
    n = 3, so I expect a 3x3 square back just like below as a string:
    
    +++
    +++
    +++

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,25 @@ internal class Program
     }
 }
 
+/*
+ I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+Example
+n = 3, so I expect a 3x3 square back just like below as a string:
+
++++
++++
++++
+ */
+
+public static class KataGenerateShape
+{
+    public static string GenerateShape(int n)
+    {
+        return string.Join("\n", Enumerable.Repeat(new string('+', n), n));
+    }
+}
+
 //Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
 
 public static class KataAlternateCase
