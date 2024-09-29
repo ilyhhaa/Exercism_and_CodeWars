b09cebb6bbf237d91de8bbfb98a43c4b8854764f
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-29 14:09:14 +0300

    Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
    You need to consider the following ratings:
    
    Terrible: tip 0%
    Poor: tip 5%
    Good: tip 10%
    Great: tip 15%
    Excellent: tip 20%
    The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
    
    "Rating not recognised" in Javascript, Python and Ruby...
    ...or null in Java
    ...or -1 in C#
    Because you're a nice person, you always round up the tip, regardless of the service.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,39 @@ internal class Program
 
     }
 }
+
+/*
+ Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
+
+You need to consider the following ratings:
+
+Terrible: tip 0%
+Poor: tip 5%
+Good: tip 10%
+Great: tip 15%
+Excellent: tip 20%
+The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
+
+"Rating not recognised" in Javascript, Python and Ruby...
+...or null in Java
+...or -1 in C#
+Because you're a nice person, you always round up the tip, regardless of the service.
+ */
+
+public class KataCalculateTip
+{
+    public static int CalculateTip(double amount, string rating) =>
+      rating.ToLower() switch
+      {
+          "terrible" => 0,
+          "poor" => (int)Math.Ceiling(amount * 0.05),
+          "good" => (int)Math.Ceiling(amount * 0.1),
+          "great" => (int)Math.Ceiling(amount * 0.15),
+          "excellent" => (int)Math.Ceiling(amount * 0.2),
+          _ => -1,
+      };
+}
+
 /*
  Introduction and warm-up (highly recommended): Playing With Lists/Arrays Series
 
