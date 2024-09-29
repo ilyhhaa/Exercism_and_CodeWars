8264e8b2b987ba19eb1a89b5feab3238bf781a9b
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-29 14:05:29 +0300

    Introduction and warm-up (highly recommended): Playing With Lists/Arrays Series
    Task
    Given an array/list of integers, find the Nth smallest element in the array.
    
    Notes
    Array/list size is at least 3.
    Array/list's numbers could be a mixture of positives , negatives and zeros.
    Repetition in array/list's numbers could occur, so don't remove duplications.
    Input >> Output Examples
    arr=[3,1,2]            n=2    ==> return 2
    arr=[15,20,7,10,4,3]   n=3    ==> return 7
    arr=[2,169,13,-5,0,-1] n=4    ==> return 2
    arr=[2,1,3,3,1,2],     n=3    ==> return 2

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,32 @@ internal class Program
 
     }
 }
+/*
+ Introduction and warm-up (highly recommended): Playing With Lists/Arrays Series
+
+Task
+Given an array/list of integers, find the Nth smallest element in the array.
+
+Notes
+Array/list size is at least 3.
+Array/list's numbers could be a mixture of positives , negatives and zeros.
+Repetition in array/list's numbers could occur, so don't remove duplications.
+Input >> Output Examples
+arr=[3,1,2]            n=2    ==> return 2 
+arr=[15,20,7,10,4,3]   n=3    ==> return 7 
+arr=[2,169,13,-5,0,-1] n=4    ==> return 2 
+arr=[2,1,3,3,1,2],     n=3    ==> return 2 
+ */
+
+class KataNthSmallest
+{
+    public static int NthSmallest(int[] arr, int pos)
+    {
+        Array.Sort(arr);
+        return arr[pos - 1];
+    }
+}
+
 
 /*
  I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
