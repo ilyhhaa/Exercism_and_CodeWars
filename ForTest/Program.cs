85bc73e66804f0a701f17aa8820069ce1c3247f3
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-30 09:39:39 +0300

    My friend John likes to go to the cinema. He can choose between system A and system B.
    System A : he buys a ticket (15 dollars) every time
    System B : he buys a card (500 dollars) and a first ticket for 0.90 times the ticket price,
    then for each additional ticket he pays 0.90 times the price paid for the previous ticket.
    Example:
    If John goes to the cinema 3 times:
    
    System A : 15 * 3 = 45
    System B : 500 + 15 * 0.90 + (15 * 0.90) * 0.90 + (15 * 0.90 * 0.90) * 0.90 ( = 536.5849999999999, no rounding for each ticket)
    John wants to know how many times he must go to the cinema so that the final result of System B, when rounded up to the next dollar, will be cheaper than System A.
    
    The function movie has 3 parameters: card (price of the card), ticket (normal price of a ticket), perc (fraction of what he paid for the previous ticket) and returns the first n such that
    
    ceil(price of System B) < price of System A.
    More examples:
    movie(500, 15, 0.9) should return 43
        (with card the total price is 634, with tickets 645)
    movie(100, 10, 0.95) should return 24
        (with card the total price is 235, with tickets 240)

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,52 @@ internal class Program
 
     }
 }
+
+/*
+ My friend John likes to go to the cinema. He can choose between system A and system B.
+
+System A : he buys a ticket (15 dollars) every time
+System B : he buys a card (500 dollars) and a first ticket for 0.90 times the ticket price, 
+then for each additional ticket he pays 0.90 times the price paid for the previous ticket.
+Example:
+If John goes to the cinema 3 times:
+
+System A : 15 * 3 = 45
+System B : 500 + 15 * 0.90 + (15 * 0.90) * 0.90 + (15 * 0.90 * 0.90) * 0.90 ( = 536.5849999999999, no rounding for each ticket)
+John wants to know how many times he must go to the cinema so that the final result of System B, when rounded up to the next dollar, will be cheaper than System A.
+
+The function movie has 3 parameters: card (price of the card), ticket (normal price of a ticket), perc (fraction of what he paid for the previous ticket) and returns the first n such that
+
+ceil(price of System B) < price of System A.
+More examples:
+movie(500, 15, 0.9) should return 43 
+    (with card the total price is 634, with tickets 645)
+movie(100, 10, 0.95) should return 24 
+    (with card the total price is 235, with tickets 240)
+ */
+
+public class MovieC
+{
+
+    public static int Movie(int card, int ticket, double perc)
+    {
+        int visits = 0;
+        double totalCostA = 0;
+        double totalCostB = card;
+        double currentTicketPrice = ticket;
+
+        while (Math.Ceiling(totalCostB) >= totalCostA)
+        {
+            visits++;
+            totalCostA += ticket;
+            currentTicketPrice *= perc;
+            totalCostB += currentTicketPrice;
+        }
+
+        return visits;
+    }
+}
+
 /*
  The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
 
