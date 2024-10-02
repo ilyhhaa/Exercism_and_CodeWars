edc256404f84cac37af32eac6dd2981a0f1292a0
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-02 18:02:30 +0300

    Create a function that takes a number as an argument and returns a grade based on that number.
    Score Grade
    Anything greater than 1 or less than 0.6 "F"
    0.9 or greater "A"
    0.8 or greater "B"
    0.7 or greater "C"
    0.6 or greater "D"
    Examples:
    
    grader(0)   should be "F"
    grader(1.1) should be "F"
    grader(0.9) should be "A"
    grader(0.8) should be "B"
    grader(0.7) should be "C"
    grader(0.6) should be "D"

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,49 @@ internal class Program
 
     }
 }
+
+/*
+ Create a function that takes a number as an argument and returns a grade based on that number.
+
+Score	Grade
+Anything greater than 1 or less than 0.6	"F"
+0.9 or greater	"A"
+0.8 or greater	"B"
+0.7 or greater	"C"
+0.6 or greater	"D"
+Examples:
+
+grader(0)   should be "F"
+grader(1.1) should be "F"
+grader(0.9) should be "A"
+grader(0.8) should be "B"
+grader(0.7) should be "C"
+grader(0.6) should be "D"
+ */
+
+public class KataGrader
+{
+    public static char Grader(double score)
+    {
+        switch (score)
+        {
+            case var _ when score > 1 || score < 0.6:
+                return 'F';
+            case var _ when score >= 0.9:
+                return 'A';
+            case var _ when score >= 0.8:
+                return 'B';
+            case var _ when score >= 0.7:
+                return 'C';
+            case var _ when score >= 0.6:
+                return 'D';
+            default:
+                return 'F';
+        }
+    }
+}
+
+
 /*
  We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
 
