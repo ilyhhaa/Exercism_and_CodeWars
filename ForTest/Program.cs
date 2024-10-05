5b1be555d49168e6a9648935e22cda7732910bfc
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-05 11:54:43 +0300

    Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
    Examples
    "312" should return "333122"
    "102269" should return "12222666666999999999"

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,24 @@ internal class Program
 
     }
 }
+
+/*
+ Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+Examples
+"312" should return "333122"
+"102269" should return "12222666666999999999"
+ */
+
+public class Digits
+{
+    public static string Explode(string s)
+    {
+        return string.Join("", s.Select(c => new String(c, int.Parse(c.ToString()))));
+    }
+}
+
+
 //Complete the function which converts hex number (given as a string) to a decimal number.
 
 public class KataHexToDec
