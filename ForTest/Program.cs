c56734fd94fddc1d7b7d2d0fe0dfd38c77d58818
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-05 11:56:35 +0300

    An NBA game runs 48 minutes (Four 12 minute quarters). Players do not typically play the full game, subbing in and out as necessary. Your job is to extrapolate a player's points per game if they played the full 48 minutes.
    Write a function that takes two arguments, ppg (points per game) and mpg (minutes per game) and returns a straight extrapolation of ppg per 48 minutes rounded to the nearest tenth. Return 0 if 0.
    
    Examples:
    
    Kata.NbaExtrap(12, 20) => 28.8
    Kata.NbaExtrap(10, 10) => 48
    Kata.NbaExtrap(5, 17)  => 14.1
    Kata.NbaExtrap(0, 0)   => 0

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -9,6 +9,24 @@ internal class Program
 
     }
 }
+/*
+ An NBA game runs 48 minutes (Four 12 minute quarters). Players do not typically play the full game, subbing in and out as necessary. Your job is to extrapolate a player's points per game if they played the full 48 minutes.
+
+Write a function that takes two arguments, ppg (points per game) and mpg (minutes per game) and returns a straight extrapolation of ppg per 48 minutes rounded to the nearest tenth. Return 0 if 0.
+
+Examples:
+
+Kata.NbaExtrap(12, 20) => 28.8
+Kata.NbaExtrap(10, 10) => 48
+Kata.NbaExtrap(5, 17)  => 14.1
+Kata.NbaExtrap(0, 0)   => 0
+ */
+
+public class KataNbaExtrap
+{
+    public static double NbaExtrap(double ppg, double mpg) => mpg == 0 ? 0 : Math.Round(48.0 / mpg * ppg, 1);
+}
+
 
 /*
  Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
