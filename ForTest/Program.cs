b9c9b92f6dc768264df8f732b07f66da14f68d40
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-08 23:27:37 +0300

    Create a function add(n)/Add(n) which returns a function that always adds n to any number
    Note for Java: the return type and methods have not been provided to make it a bit more challenging.
    
    Func<double, double> AddOne = Kata.Add(1);
    AddOne(3) => 4

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,25 @@ internal class Program
     }
 }
 
+/*
+ Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+
+Func<double, double> AddOne = Kata.Add(1);
+AddOne(3) => 4
+ */
+
+public static class KataAdd
+{
+    public static Func<double, double> Add(double n)
+    {
+        return x => x + n;
+    }
+}
+
+//
+
 public class KataCalc2
 {
     public static int Calc(string s)
