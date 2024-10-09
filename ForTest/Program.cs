1a4986750b7cdd1eb3e0f93b1affe8f3575f6229
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-09 09:47:15 +0300

    This series of katas will introduce you to basics of doing geometry with computers.
    Point objects have fields X and Y.
    
    Write a function calculating distance between Point a and Point b.
    
    Input coordinates fit in range
    −
    50
    ⩽
    x
    ,
    y
    ⩽
    50
    −50⩽x,y⩽50. Tests compare expected result and actual answer with tolerance of 1e-6.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Drawing;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -9,6 +10,37 @@ internal class Program
 
     }
 }
+
+/*
+ This series of katas will introduce you to basics of doing geometry with computers.
+
+Point objects have fields X and Y.
+
+Write a function calculating distance between Point a and Point b.
+
+Input coordinates fit in range 
+−
+50
+⩽
+x
+,
+y
+⩽
+50
+−50⩽x,y⩽50. Tests compare expected result and actual answer with tolerance of 1e-6.
+
+
+ */
+public class KataDistanceBetweenPoints
+{
+    public static double DistanceBetweenPoints(Point a, Point b)
+    {
+        double deltaX = b.X - a.X;
+        double deltaY = b.Y - a.Y;
+        return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+}
+
 /*
  Nathan loves cycling.
 
