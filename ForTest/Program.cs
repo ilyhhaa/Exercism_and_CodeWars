69cd6d12623f13d86ca5a797c0c73c742301bfe4
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-09 09:50:40 +0300

    Task Given a number, Find if it is Disarium or not .
    Warm-up (Highly recommended)
    Playing With Numbers Series
    Notes
    Number passed is always Positive .
    Return the result as String
    Input >> Output Examples
    disariumNumber(89) ==> return "Disarium !!"
    Explanation:
    Since , 81 + 92 = 89 , thus output is "Disarium !!"
    disariumNumber(564) ==> return "Not !!"
    Explanation:
    Since , 51 + 62 + 43 = 105 != 564 , thus output is "Not !!"

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,35 @@ internal class Program
 
     }
 }
+/*
+ Task
+Given a number, Find if it is Disarium or not .
+
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+Number passed is always Positive .
+Return the result as String
+Input >> Output Examples
+disariumNumber(89) ==> return "Disarium !!"
+Explanation:
+Since , 81 + 92 = 89 , thus output is "Disarium !!"
+disariumNumber(564) ==> return "Not !!"
+Explanation:
+Since , 51 + 62 + 43 = 105 != 564 , thus output is "Not !!"
+ */
+
+class KataDisariumNumber
+{
+    public static string DisariumNumber(int number)
+    {
+        string numStr = number.ToString();
+        int sum = (int)numStr.Select((digit, index) => Math.Pow(char.GetNumericValue(digit), index + 1)).Sum();
+
+        return sum == number ? "Disarium !!" : "Not !!";
+    }
+}
+
 
 /*
  This series of katas will introduce you to basics of doing geometry with computers.
