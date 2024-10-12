780d474b783d24ac078f037aa7b0d378445a43c6
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-12 13:42:23 +0300

    Your Task Given an array of Boolean values and a logical operator, return a Boolean result based on sequentially applying the operator to the values in the array.
    Examples
    booleans = [True, True, False], operator = "AND"
    True AND True -> True
    True AND False -> False
    return False
    booleans = [True, True, False], operator = "OR"
    True OR True -> True
    True OR False -> True
    return True
    booleans = [True, True, False], operator = "XOR"
    True XOR True -> False
    False XOR False -> False
    return False
    Input
    an array of Boolean values (1 <= array_length <= 50)
    a string specifying a logical operator: "AND", "OR", "XOR"

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,53 @@ internal class Program
 
     }
 }
+/*
+Your Task
+Given an array of Boolean values and a logical operator, return a Boolean result based on sequentially applying the operator to the values in the array.
+
+Examples
+booleans = [True, True, False], operator = "AND"
+True AND True -> True
+True AND False -> False
+return False
+booleans = [True, True, False], operator = "OR"
+True OR True -> True
+True OR False -> True
+return True
+booleans = [True, True, False], operator = "XOR"
+True XOR True -> False
+False XOR False -> False
+return False
+Input
+an array of Boolean values (1 <= array_length <= 50)
+a string specifying a logical operator: "AND", "OR", "XOR"
+*/
+
+public class KataLogicalCalc
+{
+    public static bool LogicalCalc(bool[] array, string op)
+    {
+        bool result = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            switch (op)
+            {
+                case "AND":
+                    result = result && array[i];
+                    break;
+                case "OR":
+                    result = result || array[i];
+                    break;
+                case "XOR":
+                    result = result ^ array[i];
+                    break;
+            }
+        }
+
+        return result;
+    }
+}
 
 /*
  Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer). x and y are non-zero integers, therefore the given point never lies on the axes.
