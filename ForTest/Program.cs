adda1b3185a0a9afb54b24aa2deb842ed835cee2
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-12 13:45:43 +0300

    Description: Remove n exclamation marks in the sentence from left to right. n is positive integer.
    Examples
    remove("Hi!",1) === "Hi"
    remove("Hi!",100) === "Hi"
    remove("Hi!!!",1) === "Hi!!"
    remove("Hi!!!",100) === "Hi"
    remove("!Hi",1) === "Hi"
    remove("!Hi!",1) === "Hi!"
    remove("!Hi!",100) === "Hi"
    remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
    remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
    remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
    remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
     */

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -11,6 +11,43 @@ internal class Program
     }
 }
 /*
+ Description:
+Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+Examples
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+ */
+
+public class KataRemove
+{
+    public static string Remove(string s, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int index = s.IndexOf('!');
+            if (index == -1)
+            {
+                break;
+            }
+            s = s.Remove(index, 1);
+        }
+        return s;
+
+    }
+}
+
+
+/*
 Your Task
 Given an array of Boolean values and a logical operator, return a Boolean result based on sequentially applying the operator to the values in the array.
 
