2cca301f2807c97f71d240b365c8ca3c4a6615fa
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-15 23:51:03 +0300

    You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
    The returned value must be a string, and have "***" between each of its letters.
    
    You should not remove or add elements from/to the array.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,24 @@ internal class Program
       
     }
 }
+/*
+ You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.
+ */
+
+public class KataTwoSort
+{
+    public static string TwoSort(string[] s)
+    {
+        Array.Sort(s, StringComparer.Ordinal);
+        string first = s[0];
+        return string.Join("***", first.ToCharArray());
+    }
+}
+
 
 /*
  Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
