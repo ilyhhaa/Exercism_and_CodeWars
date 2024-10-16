cec541244b7c5436648bc247e12d127b93093b95
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-16 09:23:07 +0300

    Task Given an array/list [] of integers , Construct a product array Of same size Such That prod[i] is equal to The Product of all the elements of Arr[] except Arr[i].
    Notes
    Array/list size is at least 2 .
    
    Array/list's numbers Will be only Positives
    
    Repetition of numbers in the array/list could occur.
    
    Input >> Output Examples
    productArray ({12,20}) ==>  return {20,12}
    Explanation:
    The first element in prod [] array 20 is the product of all array's elements except the first element
    
    The second element 12 is the product of all array's elements except the second element .
    
    productArray ({1,5,2}) ==> return {10,2,5}
    Explanation:
    The first element 10 is the product of all array's elements except the first element 1
    
    The second element 2 is the product of all array's elements except the second element 5
    
    The Third element 5 is the product of all array's elements except the Third element 2.
    
    productArray ({10,3,5,6,2}) return ==> {180,600,360,300,900}
    Explanation:
    The first element 180 is the product of all array's elements except the first element 10
    
    The second element 600 is the product of all array's elements except the second element 3
    
    The Third element 360 is the product of all array's elements except the third element 5
    
    The Fourth element 300 is the product of all array's elements except the fourth element 6
    
    Finally ,The Fifth element 900 is the product of all array's elements except the fifth element 2

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -12,6 +12,71 @@ internal class Program
 }
 
 /*
+ Task
+Given an array/list [] of integers , Construct a product array Of same size Such That prod[i] is equal to The Product of all the elements of Arr[] except Arr[i].
+
+Notes
+Array/list size is at least 2 .
+
+Array/list's numbers Will be only Positives
+
+Repetition of numbers in the array/list could occur.
+
+Input >> Output Examples
+productArray ({12,20}) ==>  return {20,12}
+Explanation:
+The first element in prod [] array 20 is the product of all array's elements except the first element
+
+The second element 12 is the product of all array's elements except the second element .
+
+productArray ({1,5,2}) ==> return {10,2,5}
+Explanation:
+The first element 10 is the product of all array's elements except the first element 1
+
+The second element 2 is the product of all array's elements except the second element 5
+
+The Third element 5 is the product of all array's elements except the Third element 2.
+
+productArray ({10,3,5,6,2}) return ==> {180,600,360,300,900}
+Explanation:
+The first element 180 is the product of all array's elements except the first element 10
+
+The second element 600 is the product of all array's elements except the second element 3
+
+The Third element 360 is the product of all array's elements except the third element 5
+
+The Fourth element 300 is the product of all array's elements except the fourth element 6
+
+Finally ,The Fifth element 900 is the product of all array's elements except the fifth element 2
+ */
+
+class KataProductArray
+{
+    public static int[] ProductArray(int[] array)
+    {
+        int n = array.Length;
+        int[] result = new int[n];
+
+        int leftProduct = 1;
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = leftProduct;
+            leftProduct *= array[i];
+        }
+
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            result[i] *= rightProduct;
+            rightProduct *= array[i];
+        }
+
+        return result;
+    }
+}
+
+
+/*
  You will be given a string (x) featuring a cat 'C' and a mouse 'm'. The rest of the string will be made up of '.'.
 
 You need to find out if the cat can catch the mouse from it's current position. The cat can jump over three characters. So:
