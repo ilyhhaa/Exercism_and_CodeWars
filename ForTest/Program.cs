74e8e23ddae59011dbdf058f9ef23664f342622f
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-19 23:30:18 +0300

    Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
    Find the number of Friday 13th in the given year.
    
    Input: Year in Gregorian calendar as integer.
    
    Output: Number of Black Fridays in the year as an integer.
    
    Examples:
    
    unluckyDays(2015) == 3
    unluckyDays(1986) == 1

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,28 @@ internal class Program
       
     }
 }
+/*Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+
+Find the number of Friday 13th in the given year.
+
+Input: Year in Gregorian calendar as integer.
+
+Output: Number of Black Fridays in the year as an integer.
+
+Examples:
+
+unluckyDays(2015) == 3
+unluckyDays(1986) == 1*/
+
+public class KataUnluckyDays
+{
+    public static int UnluckyDays(int year)
+    {
+        return Enumerable.Range(1, 12)
+            .Count(month => new DateTime(year, month, 13).DayOfWeek == DayOfWeek.Friday);
+    }
+}
+
 /*
  Write a function
 
