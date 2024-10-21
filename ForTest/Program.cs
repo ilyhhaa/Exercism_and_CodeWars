04af2eacab2dca10e5c723c323d401b3320b2dcb
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-21 12:03:37 +0300

    Find the difference between the square of the sum of the first n natural numbers (1 <= n <= 100) and the sum of their squares.
    Example
    For example, when n = 10:
    
    The square of the sum of the numbers is:
    
    (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)² = 55² = 3025
    The sum of the squares of the numbers is:
    
    1² + 2² + 3² + 4² + 5² + 6² + 7² + 8² + 9² + 10² = 385
    Hence the difference between square of the sum of the first ten natural numbers and the sum of the squares of those numbers is: 3025 - 385 = 2640

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,32 @@ internal class Program
       
     }
 }
+/*
+ Find the difference between the square of the sum of the first n natural numbers (1 <= n <= 100) and the sum of their squares.
+
+Example
+For example, when n = 10:
+
+The square of the sum of the numbers is:
+
+(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)² = 55² = 3025
+The sum of the squares of the numbers is:
+
+1² + 2² + 3² + 4² + 5² + 6² + 7² + 8² + 9² + 10² = 385
+Hence the difference between square of the sum of the first ten natural numbers and the sum of the squares of those numbers is: 3025 - 385 = 2640
+ */
+public class KataDifferenceOfSquares
+{
+    public static int DifferenceOfSquares(int n)
+    {
+        int sumOfNumbers = n * (n + 1) / 2;
+        int sumOfSquares = n * (n + 1) * (2 * n + 1) / 6;
+
+        int squareOfSum = sumOfNumbers * sumOfNumbers;
+
+        return squareOfSum - sumOfSquares;
+    }
+}
 
 /*
  Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
