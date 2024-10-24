f8dd5f1f7a92f731c7edb6f8033de8118446b2f7
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-24 09:08:17 +0300

    You have stumbled across the divine pleasure that is owning a dog and a garden. Now time to pick up all the cr@p! :D
    Given a 2D array to represent your garden, you must find and collect all of the dog cr@p - represented by '@'.
    
    You will also be given the number of bags you have access to (bags), and the capactity of a bag (cap). If there are no bags then you can't pick anything up, so you can ignore cap.
    
    You need to find out if you have enough capacity to collect all the cr@p and make your garden clean again.
    
    If you do, return 'Clean', else return 'Cr@p'.
    
    Watch out though - if your dog is out there ('D'), he gets very touchy about being watched. If he is there you need to return 'Dog!!'.
    
    For example:
    
    bags = 2
    cap = 2
    x (or garden) =
    [[ _ , _ , _ , _ , _ , _ ],
     [ _ , _ , _ , _ , @ , _ ],
     [ @ , _ , _ , _ , _ , _ ]]
    returns 'Clean'

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,58 @@ internal class Program
       
     }
 }
+
+/*
+ You have stumbled across the divine pleasure that is owning a dog and a garden. Now time to pick up all the cr@p! :D
+
+Given a 2D array to represent your garden, you must find and collect all of the dog cr@p - represented by '@'.
+
+You will also be given the number of bags you have access to (bags), and the capactity of a bag (cap). If there are no bags then you can't pick anything up, so you can ignore cap.
+
+You need to find out if you have enough capacity to collect all the cr@p and make your garden clean again.
+
+If you do, return 'Clean', else return 'Cr@p'.
+
+Watch out though - if your dog is out there ('D'), he gets very touchy about being watched. If he is there you need to return 'Dog!!'.
+
+For example:
+
+bags = 2
+cap = 2
+x (or garden) =
+[[ _ , _ , _ , _ , _ , _ ],
+ [ _ , _ , _ , _ , @ , _ ],
+ [ @ , _ , _ , _ , _ , _ ]]
+returns 'Clean'
+ */
+
+public class KataCrap
+{
+    public static string Crap(char[,] x, int bags, int cap)
+    {
+
+        int totalCapacity = bags * cap;
+        int crapCount = 0;
+
+        for (int i = 0; i < x.GetLength(0); i++)
+        {
+            for (int j = 0; j < x.GetLength(1); j++)
+            {
+                if (x[i, j] == 'D')
+                {
+                    return "Dog!!";
+                }
+                else if (x[i, j] == '@')
+                {
+                    crapCount++;
+                }
+            }
+        }
+
+        return crapCount <= totalCapacity ? "Clean" : "Cr@p";
+    }
+}
+
 /*
  Your task is to write a function toLeetSpeak that converts a regular english sentence to Leetspeak.
 
@@ -52,7 +104,7 @@ In this kata we use a simple LeetSpeak dialect. Use this alphabet:
 }
  */
 
-public class Kata
+public class KataToLeetSpeak
 {
     public static string ToLeetSpeak(string str)
     {
