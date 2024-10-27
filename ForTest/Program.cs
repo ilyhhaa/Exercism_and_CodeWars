f2276dcc567232dddc8ce48027a2afd6016da663
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-27 08:16:43 +0300

    This kata is the first of a sequence of four about "Squared Strings".
    You are given a string of n lines, each substring being n characters long: For example:
    
    s = "abcd\nefgh\nijkl\nmnop"
    
    We will study some transformations of this square of strings.
    
    Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
    vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
    Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
     hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
    or printed:
    
    vertical mirror   |horizontal mirror
    abcd --> dcba     |abcd --> mnop
    efgh     hgfe     |efgh     ijkl
    ijkl     lkji     |ijkl     efgh
    mnop     ponm     |mnop     abcd
    Task:
    Write these two functions
    and
    
    high-order function oper(fct, s) where
    
    fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
    
    Examples:
    s = "abcd\nefgh\nijkl\nmnop"
    oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
    oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd"
    Note:
    The form of the parameter fct in oper changes according to the language. You can see each form according to the language in "Sample Tests".

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -12,6 +12,60 @@ internal class Program
 }
 
 /*
+ This kata is the first of a sequence of four about "Squared Strings".
+
+You are given a string of n lines, each substring being n characters long: For example:
+
+s = "abcd\nefgh\nijkl\nmnop"
+
+We will study some transformations of this square of strings.
+
+Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
+vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
+Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
+ hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
+or printed:
+
+vertical mirror   |horizontal mirror   
+abcd --> dcba     |abcd --> mnop 
+efgh     hgfe     |efgh     ijkl 
+ijkl     lkji     |ijkl     efgh 
+mnop     ponm     |mnop     abcd 
+Task:
+Write these two functions
+and
+
+high-order function oper(fct, s) where
+
+fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+
+Examples:
+s = "abcd\nefgh\nijkl\nmnop"
+oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
+oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd"
+Note:
+The form of the parameter fct in oper changes according to the language. You can see each form according to the language in "Sample Tests".
+ */
+
+public class Opstrings
+{
+    public static string VertMirror(string strng)
+    {
+        return string.Join("\n", strng.Split('\n').Select(s => new string(s.Reverse().ToArray())));
+    }
+
+    public static string HorMirror(string strng)
+    {
+        return string.Join("\n", strng.Split('\n').Reverse());
+    }
+
+    public static string Oper(Func<string, string> fct, string s)
+    {
+        return fct(s);
+    }
+}
+
+/*
  A stream of data is received and needs to be reversed.
 
 Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
