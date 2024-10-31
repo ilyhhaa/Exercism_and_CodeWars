67fb7de6da4564308f9009bcdf6dc597e91a79af
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-10-31 09:30:07 +0300

    Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
    If two words have the same last letter, the returned array should show them in the order they appeared in the given string.
    
    All inputs will be valid.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -12,6 +12,23 @@ internal class Program
 }
 
 /*
+ Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, the returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid.
+ */
+
+public static class Kata
+{
+    public static string[] Last(string x)
+    {
+        return x.Split(' ').OrderBy(word => word.Last()).ToArray();
+    }
+}
+
+
+/*
  Let us begin with an example:
 
 Take a number: 56789. Rotate left, you get 67895.
