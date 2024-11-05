c1f269c48c00fa92d168e543927817e9520acca3
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-11-05 11:56:03 +0300

    Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
    Your task is to write a function maskify, which changes all but the last four characters into '#'.
    
    Examples (input --> output):
    "4556364607935616" --> "############5616"
         "64607935616" -->      "#######5616"
                   "1" -->                "1"
                    "" -->                 ""
    
    // "What was the name of your first pet?"
    "Skippy" --> "##ippy"
    "Nananananananananananananananana Batman!" --> "####################################man!"

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,41 @@ internal class Program
 
     }
 }
+/*
+ 
+ Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+Examples (input --> output):
+"4556364607935616" --> "############5616"
+     "64607935616" -->      "#######5616"
+               "1" -->                "1"
+                "" -->                 ""
+
+// "What was the name of your first pet?"
+"Skippy" --> "##ippy"
+"Nananananananananananananananana Batman!" --> "####################################man!"
+ */
+
+public static class KataMaskify
+{
+
+    public static string Maskify(string cc)
+    {
+        if (cc.Length <= 4)
+        {
+            return cc;
+        }
+
+        int maskedLength = cc.Length - 4;
+        string masked = new string('#', maskedLength);
+        string lastFour = cc.Substring(cc.Length - 4);
+        return masked + lastFour;
+
+    }
+}
+
 
 /*
  This Kata is intended as a small challenge for my students
@@ -31,7 +66,7 @@ For example:
 323500 --> "89 hour(s) and 51 minute(s)"
  */
 
-public static class Kata
+public static class KataExecute
 {
     public static double? Execute(double num1, char op, double num2)
     {
