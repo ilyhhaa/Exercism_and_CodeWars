a50fdd3f7bf4da29f7676e2a3389ea3a7d5cb990
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-11-06 14:19:09 +0300

    Build Tower Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
    For example, a tower with 3 floors looks like this:
    
    [
      "  *  ",
      " *** ",
      "*****"
    ]
    And a tower with 6 floors looks like this:
    
    [
      "     *     ",
      "    ***    ",
      "   *****   ",
      "  *******  ",
      " ********* ",
      "***********"
    ]

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -11,6 +11,43 @@ internal class Program
     }
 }
 /*
+ Build Tower
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+]
+ */
+public class Kata
+{
+    public static string[] TowerBuilder(int nFloors)
+    {
+        string[] tower = new string[nFloors];
+        int width = nFloors * 2 - 1; for (int i = 0; i < nFloors; i++)
+        {
+            int stars = i * 2 + 1; int spaces = (width - stars) / 2;
+            tower[i] = new string(' ', spaces) + new string('*', stars) + new string(' ', spaces);
+        }
+
+        return tower;
+    }
+}
+
+/*
  Define a method/function that removes from a given array of integers all the values contained in a second array.
 
 Examples (input -> output):
