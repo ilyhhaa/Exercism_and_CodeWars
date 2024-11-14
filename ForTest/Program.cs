22105c109f27770adba94fb436f132268fddb402
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-11-14 16:55:55 +0300

    Create a public class called Cube without a constructor which gets one single private integer variable Side, a getter GetSide() and a setter SetSide(int num) method for this property. Actually, getter and setter methods are not the common way to write this code in C#. In the next kata of this series, we gonna refactor the code and make it a bit more professional...
    Notes:
    
    There's no need to check for negative values!
    initialise the side to 0.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -12,6 +12,30 @@ internal class Program
 }
 
 /*
+ Create a public class called Cube without a constructor which gets one single private integer variable Side, a getter GetSide() and a setter SetSide(int num) method for this property. Actually, getter and setter methods are not the common way to write this code in C#. In the next kata of this series, we gonna refactor the code and make it a bit more professional...
+
+Notes:
+
+There's no need to check for negative values!
+initialise the side to 0.
+ */
+
+public class Cube 
+{
+    private int side = 0;
+
+    public int GetSide()
+    {
+        return side;
+    }
+    public void SetSide(int num)
+    {
+        side = num;
+    }
+}
+
+
+/*
  * Write a function that removes every lone 9 that is inbetween 7s.
 
 "79712312" --> "7712312"
