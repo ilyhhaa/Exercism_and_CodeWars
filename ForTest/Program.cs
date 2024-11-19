dede50a225dcbbbd218e74703f4de0d14fa89ced
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-11-19 21:04:10 +0300

    Complete the solution so that it takes the object (JavaScript/CoffeeScript) or hash (ruby) passed in and generates a human readable string from its key/value pairs.
    The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
    
    Example:
    
    Kata.StringifyDict(new Dictionary<char, int> {{'a', 1}, {'b', 2}}) => "a = 1,b = 2";

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -11,6 +11,25 @@ internal class Program
     }
 }
 
+/*
+ Complete the solution so that it takes the object (JavaScript/CoffeeScript) or hash (ruby) passed in and generates a human readable string from its key/value pairs.
+
+The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+
+Example:
+
+Kata.StringifyDict(new Dictionary<char, int> {{'a', 1}, {'b', 2}}) => "a = 1,b = 2";
+ */
+
+public static class KataStringifyDict
+{
+    public static string StringifyDict<TKey, TValue>(Dictionary<TKey, TValue> hash)
+    {
+        return string.Join(",", hash.Select(kv => $"{kv.Key} = {kv.Value}"));
+    }
+}
+
+
 
 /*
  Every now and then people in the office moves teams or departments. Depending what people are doing with their time they can become more or less boring. Time to assess the current team.
