a48c5b37dc29e83af8acd2b0c5a47612fbc23067
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-11-19 21:02:25 +0300

    Every now and then people in the office moves teams or departments. Depending what people are doing with their time they can become more or less boring. Time to assess the current team.
    You will be provided with an object(staff) containing the staff names as keys, and the department they work in as values.
    
    Each department has a different boredom assessment score, as follows:
    
    accounts = 1
    finance = 2
    canteen = 10
    regulation = 3
    trading = 6
    change = 6
    IS = 8
    retail = 5
    cleaning = 4
    pissing about = 25
    
    Depending on the cumulative score of the team, return the appropriate sentiment:
    
    <=80: 'kill me now'
    < 100 & > 80: 'i can handle this'
    100 or over: 'party time!!'

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,53 @@ internal class Program
 
     }
 }
+
+
+/*
+ Every now and then people in the office moves teams or departments. Depending what people are doing with their time they can become more or less boring. Time to assess the current team.
+
+You will be provided with an object(staff) containing the staff names as keys, and the department they work in as values.
+
+Each department has a different boredom assessment score, as follows:
+
+accounts = 1
+finance = 2
+canteen = 10
+regulation = 3
+trading = 6
+change = 6
+IS = 8
+retail = 5
+cleaning = 4
+pissing about = 25
+
+Depending on the cumulative score of the team, return the appropriate sentiment:
+
+<=80: 'kill me now'
+< 100 & > 80: 'i can handle this'
+100 or over: 'party time!!'
+ */
+public class KataBoredom
+{
+    public static string Boredom(Dictionary<string, string> staff)
+    {
+        var boredomScores = new Dictionary<string, int>
+        {
+            { "accounts", 1 }, { "finance", 2 }, { "canteen", 10 }, { "regulation", 3 }, { "trading", 6 },
+            { "change", 6 }, { "IS", 8 }, { "retail", 5 }, { "cleaning", 4 }, { "pissing about", 25 }
+        };
+
+        int totalScore = staff.Values.Sum(department => boredomScores.GetValueOrDefault(department, 0));
+
+        return totalScore switch
+        {
+            <= 80 => "kill me now",
+            < 100 => "i can handle this",
+            _ => "party time!!"
+        };
+    }
+}
+
 /*
  Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
 
