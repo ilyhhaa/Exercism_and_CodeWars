64e0133d48b4f86265f496fe2549bef777fb4bc3
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-11-22 11:27:13 +0300

    Input:
    a string strng
    an array of strings arr
    Output of function contain_all_rots(strng, arr) (or containAllRots or contain-all-rots):
    
    a boolean true if all rotations of strng are included in arr
    false otherwise
    Examples:
    contain_all_rots(
      "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
    
    contain_all_rots(
      "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
    Note:
    Though not correct in a mathematical sense
    
    we will consider that there are no rotations of strng == ""
    and for any array arr: contain_all_rots("", arr) --> true

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,44 @@ internal class Program
 
     }
 }
+/*
+ Input:
+
+a string strng
+an array of strings arr
+Output of function contain_all_rots(strng, arr) (or containAllRots or contain-all-rots):
+
+a boolean true if all rotations of strng are included in arr
+false otherwise
+Examples:
+contain_all_rots(
+  "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
+
+contain_all_rots(
+  "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
+Note:
+Though not correct in a mathematical sense
+
+we will consider that there are no rotations of strng == ""
+and for any array arr: contain_all_rots("", arr) --> true
+ */
+
+public class Rotations
+{
+    public static bool ContainAllRots(string strng, List<string> arr)
+    {
+        if (strng == "")
+            return true;
+
+        for (int i = 0; i < strng.Length; i++)
+        {
+            string rotation = strng.Substring(i) + strng.Substring(0, i);
+            if (!arr.Contains(rotation))
+                return false;
+        }
+        return true;
+    }
+}
 
 
 /*
