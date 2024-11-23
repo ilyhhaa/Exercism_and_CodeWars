f0a76c3a9802da96f1fcef27829923f5ade77778
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-11-23 22:07:20 +0300

    It is guaranteed that a and b are both present in arr.

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -11,7 +11,26 @@ internal class Program
     }
 }
 
+/*
+ You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
 
+It is guaranteed that a and b are both present in arr.
+ */
+
+public static class KataConsecutive
+{
+    public static bool Consecutive(int[] arr, int a, int b)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            if ((arr[i] == a && arr[i + 1] == b) || (arr[i] == b && arr[i + 1] == a))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
 
 /*
  Write a function that returns the number of occurrences of an element in an array.
