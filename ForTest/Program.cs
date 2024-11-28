82a429e8bc237368b9fbfb67f83180f91c989294
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-11-28 13:23:09 +0300

    public static string ShorterReverseLonger(string a, string b)   {     a ??= "";     b ??= "";     return b.Length > a.Length ? a + string.Concat(b.Reverse()) + a : b + string.Concat(a.Reverse()) + b;   }

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -11,6 +11,7 @@ internal class Program
     }
 }
 
+
 /*
  Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
 
@@ -18,6 +19,15 @@ In other words, the shortest string has to be put as prefix and as suffix of the
 
 Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
 If a and b have the same length treat a as the longer producing b+reverse(a)+b
+ */
+
+/*
+  public static string ShorterReverseLonger(string a, string b)
+  {
+    a ??= "";
+    b ??= "";
+    return b.Length > a.Length ? a + string.Concat(b.Reverse()) + a : b + string.Concat(a.Reverse()) + b;
+  }
  */
 public class ReverseLonger
 {
