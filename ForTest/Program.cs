d0d0f4e93bf5155018844b01dd09b8f439bf0fa7
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-11-28 13:24:36 +0300

    Imagine you are creating a game where the user has to guess the correct number. But there is a limit of how many guesses the user can do.
    If the user tries to guess more than the limit, the function should throw an error.
    If the user guess is right it should return true.
    If the user guess is wrong it should return false and lose a life.
    Can you finish the game so all the rules are met?

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,44 @@ internal class Program
 
     }
 }
+/*
+ Imagine you are creating a game where the user has to guess the correct number. But there is a limit of how many guesses the user can do.
+
+If the user tries to guess more than the limit, the function should throw an error.
+If the user guess is right it should return true.
+If the user guess is wrong it should return false and lose a life.
+Can you finish the game so all the rules are met?
+ */
+
+public class Guesser
+{
+    private int number;
+    private int lives;
+
+    public Guesser(int number, int lives)
+    {
+        this.number = number;
+        this.lives = lives;
+    }
+
+    public bool Guess(int n)
+    {
+        if (lives <= 0)
+        {
+            throw new InvalidOperationException("No lives left. Game over!");
+        }
+
+        if (n == number)
+        {
+            return true;
+        }
+        else
+        {
+            lives--;
+            return false;
+        }
+    }
+}
 
 
 /*
