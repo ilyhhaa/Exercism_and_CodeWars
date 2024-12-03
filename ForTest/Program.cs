23fa3bb1b62d83920a199c21d3a3cbc49e88ff34
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-12-03 10:50:16 +0300

    QckSort

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -11,8 +11,41 @@ internal class Program
     }
 }
 
+class QckSort
+{
+    static void QuickSort(int[] array, int left, int right)
+    { 
+        if (left < right)
+        {
+            int pivotIndex = Partition(array, left, right);
+            QuickSort(array, left, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, right);
+        }
+    }
 
-class BubbleSort
+    static int Partition(int[] array, int left, int right)
+    { int pivot = array[right]; 
+        int i = left - 1;
+        for (int j = left; j < right; j++)
+        {
+            if (array[j] <= pivot) 
+            { i++; Swap(array, i, j); }
+        } 
+        
+        Swap(array, i + 1, right);
+        
+        return i + 1; 
+    }
+    static void Swap(int[] array, int a, int b)
+    {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+
+    //BubbleSort
+    class BubbleSort
 {
     static void BubbleSorting(int[] array)
     {
