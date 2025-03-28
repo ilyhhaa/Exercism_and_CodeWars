940fcd4de6574583e7940d0f35abca9fd6010f30
Ilya <ilyhhaa@gmail.com>
2025-03-28 08:17:30 +0300

    LongestVowelChain

--- ForTest/Program.cs
+++ ForTest/Program.cs
@@ -10,6 +10,41 @@ internal class Program
 
     }
 }
+/*
+The vowel substrings in the word codewarriors are o,e,a,io. The longest of these has a length of 2. Given a lowercase string that has alphabetic characters only (both vowels and consonants) and no spaces, return the length of the longest vowel substring. Vowels are any of aeiou.
+*/
+public static class LongestVowelChain
+{
+  public static int Solve(string str)
+{
+    int currentLength = 0; 
+    int maxLength = 0;     
+    string vowels = "aeiou"; 
+
+    
+    foreach (char c in str)
+    {
+        
+        if (vowels.Contains(c))
+        {
+            currentLength++;
+        }
+        else
+        {
+            
+            currentLength = 0;
+        }
+
+        
+        if (currentLength > maxLength)
+        {
+            maxLength = currentLength;
+        }
+    }
+
+    return maxLength; 
+}
+}
 
 class MergeSort
 {
